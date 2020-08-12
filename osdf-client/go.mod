da0ab92648d0a8370d4d1ab36e381998112bfd2c
Salman Djingueinabaye <sdjingueinabaye2@husker.unl.edu>
2020-08-12 13:57:15 -0500

    Implemented cvmfs download

--- osdf-client/go.mod
+++ osdf-client/go.mod
@@ -3,4 +3,5 @@ module github.com/salmandjing/stashcp
 go 1.14
 
 require github.com/cavaliercoder/grab v2.0.0+incompatible
-require github.com/termie/go-shutil 
+// require github.com/termie/go-shutil v1.2.3+incompatible
+// require github.com/jcelliott/lumber v1.2.3+incompatible
--- osdf-client/handle_cvmfs.go
+++ osdf-client/handle_cvmfs.go
@@ -5,29 +5,40 @@ import (
 	"os",
 	"path"
 	shutil "github.com/termie/go-shutil"
+	"time"
+	lumber "github.com/jcelliott/lumber"
 )
+
+type payload struct {
+	tries int
+	cache string
+	host string
+}
 
 func download_cvmfs(sourceFile string, destination string, debug string, payload map[string]int) {
 	//Check if file is available in cvfms
 	var cvmfs_file string = path.Join("/cvmfs/stash.osgstorage.org", sourceFile)
-	// TODO:  logging.debug("Checking if the CVMFS file exists: %s", cvmfs_file)
+	
+	// Log
+	log:= lumber.NewConsoleLogger(lumber.WARN)
+	log.Debug("Checking if the CVMFS file exists: %s", cvmfs_file)
 
 	if _, err := os.Stat(cvmfs_file); !os.IsNotExist(err) {
+		
 		// If path exists
 		shutil.CopyFile(sourceFile, destination)
-		// TODO:  logging.debug("Succesfully copied file from CVMFS!")
-		// ????:  end1 = int(time.time()*1000) is never used
+		log.Debug("Succesfully copied file from CVMFS!")
 
-		// ????: Map only supports 1 basic type so what should I do with ints
-		payload["tries"] = "1"
-		payload["cache"] = "CVMFS"
-		payload["host"] = "CVMFS"
-		
-		//????: How to implement try catch
-		// TODO : logging.warning("Unable to copy with CVMFS, even though file exists: %s", str(e))
-		//???? how to implement  IOError as e:
+		var end1 int64 = int32(time.Now().Unix())
+
+		payload := paylo{tries: 1, cache: "CVMFS", host:"CVMFS"}
+
+		if err != nill{
+			log.Warn("Unable to copy with CVMFS, even though file exists: %s", err
+		}
 
 	}else {
-		// TODO  logging.debug("CVMFS File does not exist")
+		 log.Debug("CVMFS File does not exist")
 	}
+	
 }
