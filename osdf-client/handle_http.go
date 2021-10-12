2dfc4a9f3d18cb8abe01055fbb152198fe53d988
Derek Weitzel <djw8605@gmail.com>
2021-10-11 20:15:24 -0500

    Add proxy to accumulated errors

--- osdf-client/handle_http.go
+++ osdf-client/handle_http.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"path"
 	"regexp"
+	"strconv"
 	"sync/atomic"
 	"time"
 
@@ -135,7 +136,9 @@ func download_http(source string, destination string, payload *payloadStruct, na
 		log.Debugln("Constructed URL:", transfer.Url.String())
 		if err := DownloadHTTP(transfer, destination, token); err != nil {
 			log.Debugln("Failed to download:", err)
-			toAccum := errors.New("Failed to download from " + transfer.Url.String() + ": " + err.Error() + ";")
+			toAccum := errors.New("Failed to download from " + transfer.Url.String() +
+				" + proxy=" + strconv.FormatBool(transfer.Proxy) +
+				": " + err.Error())
 			AddError(toAccum)
 			continue
 		} else {
