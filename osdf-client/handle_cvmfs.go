0d14d286c1b203cb0bd5f5757b3c588cfc0d42a4
Derek Weitzel <djw8605@gmail.com>
2022-01-09 19:03:04 -0600

    Propagate the number of bytes transferred back up to the main function.
    Fixes #22

--- osdf-client/handle_cvmfs.go
+++ osdf-client/handle_cvmfs.go
@@ -9,7 +9,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func download_cvmfs(sourceFile string, destination string, payload *payloadStruct) error {
+func download_cvmfs(sourceFile string, destination string, payload *payloadStruct) (int64, error) {
 	//Check if file is available in cvfms
 	var cvmfs_file string = path.Join("/cvmfs/stash.osgstorage.org", sourceFile)
 
@@ -22,26 +22,26 @@ func download_cvmfs(sourceFile string, destination string, payload *payloadStruc
 		in, err := os.Open(cvmfs_file)
 		if err != nil {
 			log.Debugln("Failed to open the source file:", err)
-			return err
+			return 0, err
 		}
 		defer in.Close()
 
 		out, err := os.Create(destination)
 		if err != nil {
 			log.Debugln("Failed to create destination file:", err)
-			return err
+			return 0, err
 		}
 		defer out.Close()
 
 		_, err = io.Copy(out, in)
 		if err != nil {
 			log.Debugln("Copy of file failed:", err)
-			return err
+			return 0, err
 		}
 		err = out.Close()
 		if err != nil {
 			log.Debugln("Error while closing output file:", err)
-			return err
+			return 0, err
 		}
 		log.Debug("Succesfully copied file from CVMFS!")
 
@@ -51,12 +51,18 @@ func download_cvmfs(sourceFile string, destination string, payload *payloadStruc
 
 		if err != nil {
 			log.Warnf("Unable to copy with CVMFS, even though file exists: %s", err)
-			return err
+			return 0, err
 		}
 
 	} else {
 		log.Debugf("CVMFS File does not exist")
-		return errors.New("CVMFS File does not exist")
+		return 0, errors.New("CVMFS File does not exist")
 	}
-	return nil
+
+	// Get the size of the destination
+	dest_stat, err := os.Stat(destination)
+	if err != nil {
+		return 0, err
+	}
+	return dest_stat.Size(), nil
 }
