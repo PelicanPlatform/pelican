5d6812d9a1924ae97977161cce1ed7b3eb7d1371
Derek Weitzel <djw8605@gmail.com>
2022-03-11 02:17:57 +0000

    Fix tests for new transfers struct

--- osdf-client/cmd/stash_plugin/main_test.go
+++ osdf-client/cmd/stash_plugin/main_test.go
@@ -17,18 +17,18 @@ func TestReadMultiTransfer(t *testing.T) {
 	transfers, err := readMultiTransfers(*bufio.NewReader(strings.NewReader(stdin)))
 	assert.NoError(t, err)
 	assert.Equal(t, 3, len(transfers))
-	assert.Equal(t, "/path/to/local/copy/of/foo", transfers[0].destination)
-	assert.Equal(t, "url://server/some/directory//foo", transfers[0].source)
-	assert.Equal(t, "/path/to/local/copy/of/bar", transfers[1].destination)
-	assert.Equal(t, "url://server/some/directory//bar", transfers[1].source)
-	assert.Equal(t, "/path/to/local/copy/of/qux", transfers[2].destination)
-	assert.Equal(t, "url://server/some/directory//qux", transfers[2].source)
+	assert.Equal(t, "/path/to/local/copy/of/foo", transfers[0].localFile)
+	assert.Equal(t, "url://server/some/directory//foo", transfers[0].url)
+	assert.Equal(t, "/path/to/local/copy/of/bar", transfers[1].localFile)
+	assert.Equal(t, "url://server/some/directory//bar", transfers[1].url)
+	assert.Equal(t, "/path/to/local/copy/of/qux", transfers[2].localFile)
+	assert.Equal(t, "url://server/some/directory//qux", transfers[2].url)
 
 	// Test with single transfers
 	stdin = "[ LocalFileName = \"/path/to/local/copy/of/blah\"; Url = \"url://server/some/directory//blah\" ]"
 	transfers, err = readMultiTransfers(*bufio.NewReader(strings.NewReader(stdin)))
 	assert.NoError(t, err)
 	assert.Equal(t, 1, len(transfers))
-	assert.Equal(t, "url://server/some/directory//blah", transfers[0].source)
-	assert.Equal(t, "/path/to/local/copy/of/blah", transfers[0].destination)
+	assert.Equal(t, "url://server/some/directory//blah", transfers[0].url)
+	assert.Equal(t, "/path/to/local/copy/of/blah", transfers[0].localFile)
 }
