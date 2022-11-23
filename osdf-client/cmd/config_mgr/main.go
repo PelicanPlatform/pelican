212ad9e50231803256072f4a6ffe2fd1bae71f3f
Derek Weitzel <djw8605@gmail.com>
2022-11-22 20:38:47 -0600

    Convert config_mgr to use cobra command library

--- osdf-client/cmd/config_mgr/main.go
+++ osdf-client/cmd/config_mgr/main.go
@@ -2,256 +2,272 @@ package main
 
 import (
 	"fmt"
+	"github.com/spf13/cobra"
 	"os"
 
 	config "github.com/htcondor/osdf-client/v6/config"
 	log "github.com/sirupsen/logrus"
-	"github.com/jessevdk/go-flags"
 	"gopkg.in/yaml.v3"
 )
 
 var (
 	version = "dev"
-	commit  = "none"
-	date    = "unknown"
-	builtBy = "unknown"
+	/*
+		commit  = "none"
+		date    = "unknown"
+		builtBy = "unknown"
+	*/
 )
 
-type Options struct {
-	Debug bool `short:"d" long:"debug" description:"Turn on debug logging"`
-
-	Version bool `long:"version" short:"v" description:"Print the version and exit"`
-
-	Commands []string `positional-arg-name:"commands" description:"Command to run"`
-}
-
-var options Options
-var parser = flags.NewParser(&options, flags.Default)
-
-func main_config(commands []string) {
-
-	if len(commands) == 0 || commands[0] == "print" {
-		config, err := config.GetConfigContents()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Failed to get configuration contents:", err)
-			os.Exit(1)
-		}
-		config_b, err := yaml.Marshal(&config)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Failed to convert object to YAML:", err)
-			os.Exit(1)
-		}
-		fmt.Println(string(config_b))
-		return
-	}
-
-	switch verb := commands[0]; verb {
-	case "replace":
-		if len(commands) < 2 {
-			fmt.Fprintln(os.Stderr, "Must provide a filename to replace the configuration with")
-			os.Exit(1)
-		}
-		input_config_b, err := os.ReadFile(commands[1])
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Failed to read config file:", err)
-			os.Exit(1)
-		}
-
-		input_config := config.OSDFConfig{}
-		err = yaml.Unmarshal(input_config_b, &input_config)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Failed to parse config file:", err)
-			os.Exit(1)
-		}
-
-		err = config.SaveConfigContents(&input_config)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Unable to save replaced configuration file:", err)
-			os.Exit(1)
-		}
-	case "reset-password":
-		err := config.ResetPassword()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Failed to get reset password:", err)
-			os.Exit(1)
-		}
-	default:
-		fmt.Printf("Error: Unknown command '%s'\n", verb)
-		fmt.Printf("Usage: %s config [print|replace|reset-password]", os.Args[0])
+func printConfig() {
+	config, err := config.GetConfigContents()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to get configuration contents:", err)
 		os.Exit(1)
 	}
-	return
-}
-
-func main_prefix(commands []string) {
-
-	if len(commands) == 0 || commands[0] == "print" {
-		config, err := config.GetConfigContents()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Failed to get configuration contents:", err)
-			os.Exit(1)
-		}
-		clientList := &config.OSDF.OauthClient
-		config_b, err := yaml.Marshal(&clientList)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Failed to convert object to YAML:", err)
-			os.Exit(1)
-		}
-		fmt.Println(string(config_b))
-		return
-	}
-
-	switch verb := commands[0]; verb {
-	case "add":
-		if len(commands) < 2 {
-			fmt.Fprintln(os.Stderr, "Must provide a prefix name to add")
-			os.Exit(1)
-		}
-
-		input_config, err := config.GetConfigContents()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Failed to get configuration contents:", err)
-			os.Exit(1)
-		}
-
-		hasPrefix := false
-		for _, entry := range input_config.OSDF.OauthClient {
-			if entry.Prefix == commands[1] {
-				hasPrefix = true
-				break
-			}
-		}
-		if !hasPrefix {
-			newPrefix := config.PrefixEntry{Prefix: commands[1]}
-			input_config.OSDF.OauthClient = append(input_config.OSDF.OauthClient, newPrefix)
-		} else {
-			fmt.Fprintln(os.Stderr, "Prefix to add already exists")
-			return
-		}
-
-		err = config.SaveConfigContents(&input_config)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Unable to save replaced configuration file:", err)
-			os.Exit(1)
-		}
-	case "set":
-		if len(commands) < 4 {
-			fmt.Fprintln(os.Stderr, "Must provide a prefix name, attribute, and value to set")
-			os.Exit(1)
-		}
-
-		input_config, err := config.GetConfigContents()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Failed to get configuration contents:", err)
-			os.Exit(1)
-		}
-
-		var existingPrefix *config.PrefixEntry
-		existingPrefix = nil
-		for idx, _ := range input_config.OSDF.OauthClient {
-			if input_config.OSDF.OauthClient[idx].Prefix == commands[1] {
-				existingPrefix = &input_config.OSDF.OauthClient[idx]
-				break
-			}
-		}
-		if existingPrefix == nil {
-			fmt.Fprintln(os.Stderr, "Prefix to set was not present")
-			os.Exit(1)
-		}
-
-		if commands[2] == "client_id" {
-			existingPrefix.ClientID = commands[3]
-		} else if commands[2] == "client_secret" {
-			existingPrefix.ClientSecret = commands[3]
-		} else {
-			fmt.Fprintln(os.Stderr, "Unknown attribute to set:", commands[2])
-			os.Exit(1)
-		}
-
-		err = config.SaveConfigContents(&input_config)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Unable to save replaced configuration file:", err)
-			os.Exit(1)
-		}
-	case "delete":
-		if len(commands) < 2 {
-			fmt.Fprintln(os.Stderr, "Must provide a prefix to remove")
-			os.Exit(1)
-		}
-
-		input_config, err := config.GetConfigContents()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Failed to get configuration contents:", err)
-			os.Exit(1)
-		}
-
-		prefix_list := input_config.OSDF.OauthClient
-		new_prefix_list := make([]config.PrefixEntry, 0, len(prefix_list) - 1)
-		for _, entry := range prefix_list {
-			if entry.Prefix != commands[1] {
-				new_prefix_list = append(new_prefix_list, entry)
-			}
-		}
-		input_config.OSDF.OauthClient = new_prefix_list
-
-		err = config.SaveConfigContents(&input_config)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Unable to save replaced configuration file:", err)
-			os.Exit(1)
-		}
-	default:
-		fmt.Printf("Error: Unknown command '%s'\n", verb)
-		fmt.Printf("Usage: %s config [print|add|delete]", os.Args[0])
+	config_b, err := yaml.Marshal(&config)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to convert object to YAML:", err)
 		os.Exit(1)
 	}
-	return
+	fmt.Println(string(config_b))
+}
+
+func addConfigSubcommands(configCmd *cobra.Command) {
+
+	configCmd.AddCommand(&cobra.Command{
+		Use:   "print",
+		Short: "Print the configuration file",
+		Long:  "Print the configuration file",
+		Run: func(cmd *cobra.Command, args []string) {
+			printConfig()
+		},
+	})
+
+	configCmd.AddCommand(&cobra.Command{
+		Use:   "replace",
+		Short: "Replace the configuration file",
+		Long:  "Replace the configuration file",
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			input_config_b, err := os.ReadFile(args[0])
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Failed to read config file:", err)
+				os.Exit(1)
+			}
+
+			input_config := config.OSDFConfig{}
+			err = yaml.Unmarshal(input_config_b, &input_config)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Failed to parse config file:", err)
+				os.Exit(1)
+			}
+
+			err = config.SaveConfigContents(&input_config)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Unable to save replaced configuration file:", err)
+				os.Exit(1)
+			}
+		},
+	})
+
+	configCmd.AddCommand(&cobra.Command{
+		Use:   "reset-password",
+		Short: "Reset the password for the current user",
+		Long:  "Reset the password for the current user",
+		Run: func(cmd *cobra.Command, args []string) {
+			err := config.ResetPassword()
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Failed to get reset password:", err)
+				os.Exit(1)
+			}
+		},
+	})
+
+}
+
+func printOauthConfig() {
+	config, err := config.GetConfigContents()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to get configuration contents:", err)
+		os.Exit(1)
+	}
+	clientList := &config.OSDF.OauthClient
+	config_b, err := yaml.Marshal(&clientList)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to convert object to YAML:", err)
+		os.Exit(1)
+	}
+	fmt.Println(string(config_b))
+
+}
+func addPrefixSubcommands(prefixCmd *cobra.Command) {
+
+	prefixCmd.AddCommand(&cobra.Command{
+		Use:   "print",
+		Short: "Print the oauth client configuration file",
+		Long:  "Print the oauth client configuration file",
+		Run: func(cmd *cobra.Command, args []string) {
+			printOauthConfig()
+		},
+	})
+
+	prefixCmd.AddCommand(&cobra.Command{
+		Use:   "add <prefix>",
+		Short: "Add a new oauth client",
+		Long:  "Add a new oauth client",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			input_config, err := config.GetConfigContents()
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Failed to get configuration contents:", err)
+				os.Exit(1)
+			}
+
+			hasPrefix := false
+			for _, entry := range input_config.OSDF.OauthClient {
+				if entry.Prefix == args[0] {
+					hasPrefix = true
+					break
+				}
+			}
+			if !hasPrefix {
+				newPrefix := config.PrefixEntry{Prefix: args[0]}
+				input_config.OSDF.OauthClient = append(input_config.OSDF.OauthClient, newPrefix)
+			} else {
+				fmt.Fprintln(os.Stderr, "Prefix to add already exists")
+				return
+			}
+
+			err = config.SaveConfigContents(&input_config)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Unable to save replaced configuration file:", err)
+				os.Exit(1)
+			}
+		},
+	})
+
+	prefixCmd.AddCommand(&cobra.Command{
+		Use:   "set <prefix> <client_id|client_secret> <value>",
+		Short: "Set the oauth client attributes",
+		Long:  "Set the oauth client attributes (client_id or client_secret)",
+		Args:  cobra.ExactArgs(3),
+		Run: func(cmd *cobra.Command, args []string) {
+			input_config, err := config.GetConfigContents()
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Failed to get configuration contents:", err)
+				os.Exit(1)
+			}
+
+			var existingPrefix *config.PrefixEntry
+			existingPrefix = nil
+			for idx := range input_config.OSDF.OauthClient {
+				if input_config.OSDF.OauthClient[idx].Prefix == args[0] {
+					existingPrefix = &input_config.OSDF.OauthClient[idx]
+					break
+				}
+			}
+			if existingPrefix == nil {
+				fmt.Fprintln(os.Stderr, "Prefix to set was not present")
+				os.Exit(1)
+			}
+
+			if args[1] == "client_id" {
+				existingPrefix.ClientID = args[2]
+			} else if args[1] == "client_secret" {
+				existingPrefix.ClientSecret = args[2]
+			} else {
+				fmt.Fprintln(os.Stderr, "Unknown attribute to set:", args[1])
+				os.Exit(1)
+			}
+
+			err = config.SaveConfigContents(&input_config)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Unable to save replaced configuration file:", err)
+				os.Exit(1)
+			}
+		},
+	})
+
+	prefixCmd.AddCommand(&cobra.Command{
+		Use:   "delete",
+		Short: "Delete the oauth client",
+		Long:  "Delete the oauth client",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			input_config, err := config.GetConfigContents()
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Failed to get configuration contents:", err)
+				os.Exit(1)
+			}
+
+			prefix_list := input_config.OSDF.OauthClient
+			new_prefix_list := make([]config.PrefixEntry, 0, len(prefix_list)-1)
+			for _, entry := range prefix_list {
+				if entry.Prefix != args[0] {
+					new_prefix_list = append(new_prefix_list, entry)
+				}
+			}
+			input_config.OSDF.OauthClient = new_prefix_list
+
+			err = config.SaveConfigContents(&input_config)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Unable to save replaced configuration file:", err)
+				os.Exit(1)
+			}
+		},
+	})
+
 }
 
 func main() {
 
-	args, err := parser.Parse()
-	if err != nil {
-		if flagsErr, ok := err.(*flags.Error); ok && flagsErr.Type == flags.ErrHelp {
-			os.Exit(0)
+	// Define the config and prefix commands
+	configCmd := &cobra.Command{
+		Use:   "config",
+		Short: "Manage the configuration file",
+		Long:  "Manage the configuration file",
+		Run: func(cmd *cobra.Command, args []string) {
+			printConfig()
+		},
+	}
+	addConfigSubcommands(configCmd)
+
+	// Define the prefix commands
+	prefixCmd := &cobra.Command{
+		Use:   "prefix",
+		Short: "Manage the prefix configuration",
+		Long:  "Manage the prefix configuration",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println(args)
+		},
+	}
+	addPrefixSubcommands(prefixCmd)
+
+	// Add the config and prefix commands
+	var rootCmd = &cobra.Command{
+		Use:     "config_mgr",
+		Version: version,
+	}
+	var Debug bool
+	rootCmd.PersistentFlags().BoolVarP(&Debug, "debug", "d", false, "Debug output")
+	rootCmd.PersistentPreRun = func(cmd *cobra.Command, args []string) {
+		if Debug {
+			setLogging(log.DebugLevel)
 		} else {
-			log.Errorln(err)
-			os.Exit(1)
+			setLogging(log.ErrorLevel)
 		}
 	}
 
-	//var err error
-	if options.Debug {
-		setLogging(log.DebugLevel)
-	} else {
-		setLogging(log.ErrorLevel)
+	rootCmd.CompletionOptions.DisableDefaultCmd = true
+	rootCmd.AddCommand(configCmd)
+	rootCmd.AddCommand(prefixCmd)
+	err := rootCmd.Execute()
+	if err != nil {
+		log.Errorln(err)
 	}
-
-	if options.Version {
-		fmt.Println("Version:", version)
-		fmt.Println("Build Date:", date)
-		fmt.Println("Build Commit:", commit)
-		fmt.Println("Built By:", builtBy)
-		os.Exit(0)
-	}
-
-	if len(args) == 0 {
-		fmt.Printf("Usage: %s [config ...]\n", os.Args[0])
-		os.Exit(0)
-	}
-
-	switch noun := args[0]; noun {
-	case "config":
-		main_config(args[1:])
-	case "prefix":
-		main_prefix(args[1:])
-	default:
-		fmt.Printf("Error: Unknown command '%s'\n", noun)
-		os.Exit(1)
-	}
-	os.Exit(0)
 
 }
-
 
 func setLogging(logLevel log.Level) {
 	textFormatter := log.TextFormatter{}
--- osdf-client/go.mod
+++ osdf-client/go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/jessevdk/go-flags v1.5.0
 	github.com/jsipprell/keyctl v1.0.4-0.20211208153515-36ca02672b6c
 	github.com/sirupsen/logrus v1.8.1
+	github.com/spf13/cobra v1.6.1
 	github.com/stretchr/testify v1.2.2
 	github.com/studio-b12/gowebdav v0.0.0-20210203212356-8244b5a5f51a
 	github.com/vbauerster/mpb/v7 v7.1.5
