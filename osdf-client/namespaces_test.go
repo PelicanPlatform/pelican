7df686a68fc4505cd6efb1fbc23546a016b69089
Brian Bockelman <bbockelman@morgridge.org>
2023-06-03 15:09:13 -0500

    Update test to reflect new failure mode

--- osdf-client/namespaces_test.go
+++ osdf-client/namespaces_test.go
@@ -76,9 +76,7 @@ func TestMatchNamespace(t *testing.T) {
 
 	// Check for empty
 	ns, err = MatchNamespace("/does/not/exist.txt")
-	assert.NoError(t, err, "Failed to parse namespace")
-	assert.Equal(t, "", ns.Path)
-	assert.Equal(t, Namespace{}.UseTokenOnRead, ns.UseTokenOnRead)
+	assert.Error(t, err)
 
 	// Check for not private
 	ns, err = MatchNamespace("/osgconnect/public/path/to/file.txt")
