3a0f0b8e4f26b08f3ea78085243b14d3f675e780
Brian Bockelman <bbockelman@morgridge.org>
2023-07-01 21:36:16 -0500

    Cleanup failure from linter

--- osdf-client/handle_http_test.go
+++ osdf-client/handle_http_test.go
@@ -16,8 +16,6 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/spf13/viper"
 
-	"net/http/httputil"
-
 	namespaces "github.com/htcondor/osdf-client/v6/namespaces"
 	"github.com/htcondor/osdf-client/v6/config"
 )
