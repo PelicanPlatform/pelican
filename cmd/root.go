cf7c1798f4d8289153681c97ae07c6112878b000
Yuxiao Qu <yuxiao1155@gmail.com>
2023-07-24 10:50:14 -0500

    resolve a conflit in root.go

--- cmd/root.go
+++ cmd/root.go
@@ -37,6 +37,9 @@ func Execute() {
 }
 
 func init() {
+
+
+
 	cobra.OnInitialize(initConfig)
 
 	rootCmd.AddCommand(objectCmd)
@@ -73,7 +76,7 @@ func initConfig() {
 		viper.SetConfigType("yaml")
 		viper.SetConfigName("pelican.yaml")
 	}
-	if err := viper.BindPFlag("Debug", rootCmd.Flags().Lookup("debug")); err != nil {
+	if err := viper.BindPFlag("Debug", rootCmd.PersistentFlags().Lookup("debug")); err != nil {
 		panic(err)
 	}
 
