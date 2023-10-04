76cdc3914d7ad0805ddb8b3ba35f989ec2ef721b
Justin Hiemstra <jhiemstra@wisc.edu>
2023-10-04 19:30:45 +0000

    Update how version is grabbed during origin advertise

--- origin_ui/advertise.go
+++ origin_ui/advertise.go
@@ -123,7 +123,7 @@ func AdvertiseOrigin() error {
 
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Authorization", "Bearer "+token)
-	userAgent := "pelican-origin/" + pelican.ObjectClientOptions.Version
+	userAgent := "pelican-origin/" + client.ObjectClientOptions.Version
 	req.Header.Set("User-Agent", userAgent)
 
 	// We should switch this over to use the common transport, but for that to happen
