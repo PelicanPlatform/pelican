5d3a626c051a011cd5dc3c61940743c5ab753537
Brian Bockelman <bbockelman@morgridge.org>
2023-10-22 13:39:14 -0500

    Add `object share` subcommand to the CLI
    Additionally, refactor the client sharing method to take a URL (and
    perform metadata discovery as needed).

--- client/sharing_url.go
+++ client/sharing_url.go
@@ -20,31 +20,85 @@ package client
 
 import (
 	"net/url"
+	"strings"
 
 	"github.com/pelicanplatform/pelican/config"
 	"github.com/pelicanplatform/pelican/param"
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
-func CreateSharingUrl(path string, isWrite bool) (string, error) {
-	OSDFDirectorUrl := param.Federation_DirectorUrl.GetString()
-	dirResp, err := QueryDirector(path, OSDFDirectorUrl)
+func getDirectorFromUrl(objectUrl *url.URL) (string, error) {
+	configDirectorUrl := param.Federation_DirectorUrl.GetString()
+	var directorUrl string
+	if objectUrl.Scheme == "pelican" {
+		if objectUrl.Host == "" {
+			if configDirectorUrl == "" {
+				return "", errors.New("Must specify (or configure) the federation hostname with the pelican://-style URLs")
+			}
+			directorUrl = configDirectorUrl
+		} else {
+			discoveryUrl := url.URL{
+				Scheme: "https",
+				Host:   objectUrl.Host,
+			}
+			viper.Set("Federation.DirectorUrl", "")
+			viper.Set("Federation.DiscoveryUrl", discoveryUrl.String())
+			if err := config.DiscoverFederation(); err != nil {
+				return "", errors.Wrapf(err, "Failed to discover location of the director for the federation %s", objectUrl.Host)
+			}
+			if directorUrl = param.Federation_DirectorUrl.GetString(); directorUrl == "" {
+				return "", errors.Errorf("Director for the federation %s not discovered", objectUrl.Host)
+			}
+		}
+	} else if objectUrl.Scheme == "osdf" && configDirectorUrl == "" {
+		if objectUrl.Host != "" {
+			objectUrl.Path = "/" + objectUrl.Host + objectUrl.Path
+			objectUrl.Host = ""
+		}
+		viper.Set("Federation.DiscoveryUrl", "https://osg-htc.org")
+		if err := config.DiscoverFederation(); err != nil {
+			return "", errors.Wrap(err, "Failed to discover director for the OSDF")
+		}
+		if directorUrl = param.Federation_DirectorUrl.GetString(); directorUrl == "" {
+			return "", errors.Errorf("Director for the OSDF not discovered")
+		}
+	} else if objectUrl.Scheme == "" {
+		if configDirectorUrl == "" {
+			return "", errors.Errorf("Must provide a federation name for path %s (e.g., pelican://osg-htc.org/%s)", objectUrl.Path, objectUrl.Path)
+		} else {
+			directorUrl = configDirectorUrl
+		}
+	} else if objectUrl.Scheme != "" {
+		return "", errors.Errorf("Unsupported scheme for pelican: %s://", objectUrl.Scheme)
+	}
+	return directorUrl, nil
+}
+
+func CreateSharingUrl(objectUrl *url.URL, isWrite bool) (string, error) {
+	directorUrl, err := getDirectorFromUrl(objectUrl)
+	if err != nil {
+		return "", err
+	}
+	objectUrl.Path = "/" + strings.TrimPrefix(objectUrl.Path, "/")
+
+	log.Debugln("Will query director for path", objectUrl.Path)
+	dirResp, err := QueryDirector(objectUrl.Path, directorUrl)
 	if err != nil {
 		log.Errorln("Error while querying the Director:", err)
-		return "", errors.Wrapf(err, "Error while querying the director at %s", OSDFDirectorUrl)
+		return "", errors.Wrapf(err, "Error while querying the director at %s", directorUrl)
 	}
 	namespace, err := CreateNsFromDirectorResp(dirResp)
 	if err != nil {
-		return "", errors.Wrapf(err, "Unable to parse response from director at %s", OSDFDirectorUrl)
+		return "", errors.Wrapf(err, "Unable to parse response from director at %s", directorUrl)
 	}
 
 	opts := config.TokenGenerationOpts{Operation: config.TokenSharedRead}
 	if isWrite {
 		opts.Operation = config.TokenSharedWrite
 	}
-	pathUrl := url.URL{Path: path}
-	token, err := AcquireToken(&pathUrl, namespace, opts)
+	token, err := AcquireToken(objectUrl, namespace, opts)
 	if err != nil {
 		err = errors.Wrap(err, "Failed to acquire token")
 	}
--- client/sharing_url_test.go
+++ client/sharing_url_test.go
@@ -23,6 +23,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"os"
 	"strings"
 	"testing"
@@ -31,7 +32,79 @@ import (
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestDirectorGeneration(t *testing.T) {
+	returnError := false
+	returnErrorRef := &returnError
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		discoveryConfig := `{"director_endpoint": "https://location.example.com", "namespace_registration_endpoint": "https://location.example.com/namespace", "jwks_uri": "https://location.example.com/jwks"}`
+		if *returnErrorRef {
+			w.WriteHeader(http.StatusInternalServerError)
+		} else {
+			w.WriteHeader(http.StatusOK)
+			_, err := w.Write([]byte(discoveryConfig))
+			assert.NoError(t, err)
+		}
+	}
+	server := httptest.NewTLSServer(http.HandlerFunc(handler))
+	defer server.Close()
+	serverURL, err := url.Parse(server.URL)
+	require.NoError(t, err)
+
+	objectUrl := url.URL{
+		Scheme: "pelican",
+		Host:   serverURL.Host,
+		Path:   "/test/foo",
+	}
+
+	// Discovery works to get URL
+	viper.Reset()
+	viper.Set("TLSSkipVerify", true)
+	err = config.InitClient()
+	require.NoError(t, err)
+	dUrl, err := getDirectorFromUrl(&objectUrl)
+	require.NoError(t, err)
+	assert.Equal(t, dUrl, "https://location.example.com")
+
+	// Discovery URL overrides the federation config.
+	viper.Reset()
+	viper.Set("TLSSkipVerify", true)
+	viper.Set("Federation.DirectorURL", "https://location2.example.com")
+	dUrl, err = getDirectorFromUrl(&objectUrl)
+	require.NoError(t, err)
+	assert.Equal(t, dUrl, "https://location.example.com")
+
+	// Fallback to configuration if no discovery present
+	viper.Reset()
+	viper.Set("Federation.DirectorURL", "https://location2.example.com")
+	objectUrl.Host = ""
+	dUrl, err = getDirectorFromUrl(&objectUrl)
+	require.NoError(t, err)
+	assert.Equal(t, dUrl, "https://location2.example.com")
+
+	// Error if server has an error
+	viper.Reset()
+	returnError = true
+	viper.Set("TLSSkipVerify", true)
+	objectUrl.Host = serverURL.Host
+	_, err = getDirectorFromUrl(&objectUrl)
+	require.Error(t, err)
+
+	// Error if neither config nor hostname provided.
+	viper.Reset()
+	objectUrl.Host = ""
+	_, err = getDirectorFromUrl(&objectUrl)
+	require.Error(t, err)
+
+	// Error on unknown scheme
+	viper.Reset()
+	objectUrl.Scheme = "buzzard"
+	_, err = getDirectorFromUrl(&objectUrl)
+	require.Error(t, err)
+}
 
 func TestSharingUrl(t *testing.T) {
 	// Construct a local server that we can poke with QueryDirector
@@ -61,13 +134,13 @@ func TestSharingUrl(t *testing.T) {
 			assert.NoError(t, err)
 		} else if r.URL.Path == "/issuer/device_authz" {
 			w.WriteHeader(http.StatusOK)
-			err := w.Write([]byte(`{"device_code": "1234", "user_code": "5678", "interval": 1, "verification_uri": "https://example.com", "expires_in": 20}`))
-			assert.NoError(err)
+			_, err := w.Write([]byte(`{"device_code": "1234", "user_code": "5678", "interval": 1, "verification_uri": "https://example.com", "expires_in": 20}`))
+			assert.NoError(t, err)
 		} else if r.URL.Path == "/issuer/token" {
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusOK)
-			err := w.Write([]byte(`{"access_token": "token1234", "token_type": "jwt"}`))
-			assert.NoError(err)
+			_, err := w.Write([]byte(`{"access_token": "token1234", "token_type": "jwt"}`))
+			assert.NoError(t, err)
 		} else {
 			fmt.Println(r)
 			requestBytes, err := io.ReadAll(r.Body)
@@ -88,7 +161,9 @@ func TestSharingUrl(t *testing.T) {
 	assert.NoError(t, err)
 
 	// Call QueryDirector with the test server URL and a source path
-	token, err := CreateSharingUrl("/test/foo/bar", true)
+	testUrl, err := url.Parse("/test/foo/bar")
+	require.NoError(t, err)
+	token, err := CreateSharingUrl(testUrl, true)
 	assert.NoError(t, err)
 	assert.NotEmpty(t, token)
 	fmt.Println(token)
