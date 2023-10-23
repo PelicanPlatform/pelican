4170146154e80de99a43f61f08b0b3c1893dfe48
Haoming Meng <hmeng29@wisc.edu>
2023-10-23 17:01:40 +0000

    programmatically generate a struct from params.yaml

--- param/param.go
+++ param/param.go
@@ -1,3 +1,31 @@
 package param
 
+import (
+	"errors"
+
+	"github.com/spf13/viper"
+)
+
 //go:generate go run ../generate/param_generator.go
+
+var (
+	viperConfig config
+)
+
+// Unmarshal Viper config into a struct viperConfig
+func UnmarshalConfig() error {
+	err := viper.Unmarshal(&viperConfig)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Return the unmarshaled viper config struct as a pointer
+func GetUnmarshaledConfig() (*config, error) {
+	if &viperConfig == nil {
+		return nil, errors.New("Config hasn't been unmarshaled yet.")
+	}
+	return &viperConfig, nil
+}
