cc0a693ad664acd367e6777814c7f76a6628c3fe
Brian Bockelman <bbockelman@morgridge.org>
2023-11-04 10:47:25 -0500

    Basic xrootd server unit test
    Starts the server, waits for it to be available then performs the
    upload/download/delete test cycle again xrootd.

---
+++ xrootd/origin_test.go
@@ -0,0 +1,104 @@
+//go:build !windows
+
+/***************************************************************
+ *
+ * Copyright (C) 2023, Pelican Project, Morgridge Institute for Research
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ***************************************************************/
+
+package xrootd
+
+import (
+	"context"
+	"crypto/elliptic"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/pelicanplatform/pelican/config"
+	"github.com/pelicanplatform/pelican/daemon"
+	"github.com/pelicanplatform/pelican/origin_ui"
+	"github.com/pelicanplatform/pelican/param"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/require"
+)
+
+func TestOrigin(t *testing.T) {
+	viper.Reset()
+
+	viper.Set("Origin.ExportVolume", t.TempDir()+":/test")
+	// Disable functionality we're not using (and is difficult to make work on Mac)
+	viper.Set("Origin.EnableCmsd", false)
+	viper.Set("Origin.EnableMacaroons", false)
+	viper.Set("Origin.EnableVoms", false)
+	viper.Set("TLSSkipVerify", true)
+
+	viper.Set("ConfigDir", t.TempDir())
+	// Increase the log level; otherwise, its difficult to debug failures
+	viper.Set("Logging.Level", "Debug")
+	config.InitConfig()
+	err := config.InitServer()
+	require.NoError(t, err)
+
+	err = config.GeneratePrivateKey(param.Server_TLSKey.GetString(), elliptic.P256())
+	require.NoError(t, err)
+	err = config.GenerateCert()
+	require.NoError(t, err)
+
+	err = CheckXrootdEnv(true)
+	require.NoError(t, err)
+
+	err = SetUpMonitoring()
+	require.NoError(t, err)
+
+	configPath, err := ConfigXrootd(true)
+	require.NoError(t, err)
+
+	launchers, err := ConfigureLaunchers(false, configPath)
+	require.NoError(t, err)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		_ = daemon.LaunchDaemons(ctx, launchers)
+	}()
+	defer cancel()
+
+	testExpiry := time.Now().Add(10 * time.Second)
+	testSuccess := false
+	for !testSuccess || time.Now().After(testExpiry) {
+		time.Sleep(50 * time.Millisecond)
+		req, err := http.NewRequest("GET", param.Origin_Url.GetString(), nil)
+		require.NoError(t, err)
+		httpClient := http.Client{
+			Transport: config.GetTransport(),
+			Timeout:   50 * time.Millisecond,
+		}
+		_, err = httpClient.Do(req)
+		if err != nil {
+			log.Infoln("Failed to send request to XRootD; likely, server is not up (will retry in 50ms):", err)
+		} else {
+			testSuccess = true
+			log.Debugln("XRootD server appears to be functioning; will proceed with test")
+		}
+	}
+
+	url, err := origin_ui.UploadTestfile()
+	require.NoError(t, err)
+	err = origin_ui.DownloadTestfile(url)
+	require.NoError(t, err)
+	err = origin_ui.DeleteTestfile(url)
+	require.NoError(t, err)
+}
