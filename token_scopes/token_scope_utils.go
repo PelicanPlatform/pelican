304a7f5f722e56e114f734079acc3a8d97967f0a
Haoming Meng <hmeng29@wisc.edu>
2023-12-29 16:04:00 +0000

    Migrate token scope related code to `token_scopes` pkg

--- token_scopes/token_scope_utils.go
+++ token_scopes/token_scope_utils.go
@@ -1,6 +1,14 @@
 package token_scopes
 
-import "strings"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sort"
+	"strings"
+
+	"github.com/lestrrat-go/jwx/v2/jwt"
+)
 
 // Get a string representation of a list of scopes, which can then be passed
 // to the Claim builder of JWT constructor
@@ -18,4 +26,56 @@ func GetScopeString(scopes []TokenScope) (scopeString string) {
 	}
 	scopeString = strings.TrimRight(scopeString, " ")
 	return
+}
+
+// Return if expectedScopes contains the tokenScope and it's case-insensitive.
+// If all=false, it checks if the tokenScopes have any one scope in expectedScopes;
+// If all=true, it checks if tokenScopes is the same set as expectedScopes
+func ScopeContains(tokenScopes []string, expectedScopes []string, all bool) bool {
+	if !all { // Any tokenScope in desiredScopes is OK
+		for _, tokenScope := range tokenScopes {
+			for _, sc := range expectedScopes {
+				if strings.EqualFold(sc, tokenScope) {
+					return true
+				}
+			}
+		}
+		return false
+	} else { // All tokenScope must be in desiredScopes
+		if len(tokenScopes) != len(expectedScopes) {
+			return false
+		}
+		sort.Strings(tokenScopes)
+		sort.Strings(expectedScopes)
+		for i := 0; i < len(tokenScopes); i++ {
+			if tokenScopes[i] != expectedScopes[i] {
+				return false
+			}
+		}
+		return true
+	}
+}
+
+// Creates a validator that checks if a token's scope matches the given scope: expectedScopes.
+// See `scopeContains` for detailed checking mechanism
+func CreateScopeValidator(expectedScopes []string, all bool) jwt.ValidatorFunc {
+
+	return jwt.ValidatorFunc(func(_ context.Context, tok jwt.Token) jwt.ValidationError {
+		// If no scope is present, always return true
+		if len(expectedScopes) == 0 {
+			return nil
+		}
+		scope_any, present := tok.Get("scope")
+		if !present {
+			return jwt.NewValidationError(errors.New("No scope is present; required for authorization"))
+		}
+		scope, ok := scope_any.(string)
+		if !ok {
+			return jwt.NewValidationError(errors.New("scope claim in token is not string-valued"))
+		}
+		if ScopeContains(strings.Split(scope, " "), expectedScopes, all) {
+			return nil
+		}
+		return jwt.NewValidationError(errors.New(fmt.Sprint("Token does not contain any of the scopes: ", expectedScopes)))
+	})
 }
--- token_scopes/token_scope_utils_test.go
+++ token_scopes/token_scope_utils_test.go
@@ -1,6 +1,9 @@
 package token_scopes
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+)
 
 func TestGetScopeString(t *testing.T) {
 	tests := []struct {
@@ -18,6 +21,44 @@ func TestGetScopeString(t *testing.T) {
 			got := GetScopeString(tt.scopes)
 			if got != tt.want {
 				t.Errorf("GetScopeString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func largeInputSet() []string {
+	var scopes []string
+	for i := 0; i < 1000; i++ {
+		scopes = append(scopes, "scope"+strconv.Itoa(i))
+	}
+	return scopes
+}
+
+func TestScopeContains(t *testing.T) {
+	tests := []struct {
+		name           string
+		tokenScopes    []string
+		expectedScopes []string
+		all            bool
+		want           bool
+	}{
+		{"empty-scopes", []string{}, []string{}, false, false},
+		{"single-match", []string{"read"}, []string{"read"}, false, true},
+		{"no-match", []string{"read"}, []string{"write"}, false, false},
+		{"multiple-matches", []string{"read", "write"}, []string{"read", "write"}, false, true},
+		{"partial-match-all-false", []string{"read", "write"}, []string{"read"}, false, true},
+		{"partial-match-all-true", []string{"read", "write"}, []string{"read"}, true, false},
+		{"case-insensitivity", []string{"Read"}, []string{"read"}, false, true},
+		{"different-lengths-all-true", []string{"read", "write"}, []string{"read"}, true, false},
+		{"exact-match-all-true", []string{"read", "write"}, []string{"write", "read"}, true, true},
+		{"large-input-sets", largeInputSet(), largeInputSet(), false, true},
+		{"nil-inputs", nil, nil, false, false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ScopeContains(tt.tokenScopes, tt.expectedScopes, tt.all); got != tt.want {
+				t.Errorf("ScopeContains() = %v, want %v", got, tt.want)
 			}
 		})
 	}
