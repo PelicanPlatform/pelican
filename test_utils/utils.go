3026395ab943fe428c22de9dbeb2634927adaf24
Haoming Meng <hmeng29@wisc.edu>
2024-01-26 16:56:10 +0000

    Add tests

--- test_utils/utils.go
+++ test_utils/utils.go
@@ -1,6 +1,6 @@
 /***************************************************************
  *
- * Copyright (C) 2023, Pelican Project, Morgridge Institute for Research
+ * Copyright (C) 2024, Pelican Project, Morgridge Institute for Research
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you
  * may not use this file except in compliance with the License.  You may
@@ -20,8 +20,12 @@ package test_utils
 
 import (
 	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"encoding/json"
 	"testing"
 
+	"github.com/lestrrat-go/jwx/v2/jwk"
 	"github.com/pelicanplatform/pelican/config"
 	"golang.org/x/sync/errgroup"
 )
@@ -35,4 +39,42 @@ func TestContext(ictx context.Context, t *testing.T) (ctx context.Context, cance
 	egrp, ctx = errgroup.WithContext(ctx)
 	ctx = context.WithValue(ctx, config.EgrpKey, egrp)
 	return
+}
+
+// GenerateJWK generates a JWK private key and a corresponding JWKS public key,
+// and the string representation of the public key
+func GenerateJWK() (jwk.Key, jwk.Set, string, error) {
+	// Generate an RSA private key
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, nil, "", err
+	}
+
+	// Create a JWK from the private key
+	jwkKey, err := jwk.FromRaw(privateKey)
+	if err != nil {
+		return nil, nil, "", err
+	}
+	_ = jwkKey.Set(jwk.KeyIDKey, "mykey")
+	_ = jwkKey.Set(jwk.AlgorithmKey, "RS256")
+	_ = jwkKey.Set(jwk.KeyUsageKey, "sig")
+
+	// Extract the public key
+	publicKey, err := jwk.PublicKeyOf(jwkKey)
+	if err != nil {
+		return nil, nil, "", err
+	}
+
+	// Create a JWKS from the public key
+	jwks := jwk.NewSet()
+	if err := jwks.AddKey(publicKey); err != nil {
+		return nil, nil, "", err
+	}
+
+	jwksBytes, err := json.Marshal(jwks)
+	if err != nil {
+		return nil, nil, "", err
+	}
+
+	return jwkKey, jwks, string(jwksBytes), nil
 }
