ea65a38a3fba308c81da593b11358c7f2f9571ce
Justin Hiemstra <jhiemstra@wisc.edu>
2024-03-01 15:16:48 +0000

    Update S3 test to use path-style URL

--- xrootd/origin_test.go
+++ xrootd/origin_test.go
@@ -210,6 +210,7 @@ func TestS3OriginConfig(t *testing.T) {
 	viper.Set("Origin.S3Bucket", bucketName)
 	viper.Set("Origin.S3ServiceName", serviceName)
 	viper.Set("Origin.S3ServiceUrl", fmt.Sprintf("http://%s", endpoint))
+	viper.Set("Origin.S3UrlStyle", "path")
 	// Disable functionality we're not using (and is difficult to make work on Mac)
 	viper.Set("Origin.EnableCmsd", false)
 	viper.Set("Origin.EnableMacaroons", false)
