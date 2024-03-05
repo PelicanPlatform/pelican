9abbdd5298b5703eb0e9b626eeb4c4e964bdd37e
Brian Bockelman <bbockelman@morgridge.org>
2024-03-05 09:52:58 -0600

    Fix compilation on Windows

--- local_cache/local_cache.go
+++ local_cache/local_cache.go
@@ -32,10 +32,8 @@ import (
 	"slices"
 	"sync"
 	"sync/atomic"
-	"syscall"
 	"time"
 
-	"github.com/alecthomas/units"
 	"github.com/google/uuid"
 	"github.com/pelicanplatform/pelican/client"
 	"github.com/pelicanplatform/pelican/common"
@@ -227,22 +225,9 @@ func NewLocalCache(ctx context.Context, egrp *errgroup.Group) (sc *LocalCache, e
 		return
 	}
 
-	sizeStr := param.LocalCache_Size.GetString()
-	var cacheSize uint64
-	if sizeStr == "" || sizeStr == "0" {
-		var stat syscall.Statfs_t
-		if err = syscall.Statfs(cacheDir, &stat); err != nil {
-			err = errors.Wrapf(err, "unable to determine free space for cache directory %s", cacheDir)
-			return
-		}
-		cacheSize = stat.Bavail * uint64(stat.Bsize)
-	} else {
-		var signedCacheSize int64
-		signedCacheSize, err = units.ParseStrictBytes(param.LocalCache_Size.GetString())
-		if err != nil {
-			return
-		}
-		cacheSize = uint64(signedCacheSize)
+	cacheSize, err := getCacheSize(cacheDir)
+	if err != nil {
+		return
 	}
 	highWaterPercentage := param.LocalCache_HighWaterMarkPercentage.GetInt()
 	lowWaterPercentage := param.LocalCache_LowWaterMarkPercentage.GetInt()
