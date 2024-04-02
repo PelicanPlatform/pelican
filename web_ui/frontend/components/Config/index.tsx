1649ad9ac893fc004ccbdbcfd0f391bfdd09d32c
Cannon Lock <clock@wisc.edu>
2024-04-02 15:49:28 -0500

    Add Lots Object
    - Add LotsField
    - Add DateTimeField
    - Add PathField

--- web_ui/frontend/components/Config/index.tsx
+++ web_ui/frontend/components/Config/index.tsx
@@ -5,6 +5,7 @@ import StringSliceField from "./StringSliceField";
 import IntegerField from "./IntegerField";
 import MultiSelectField from "./MultiSelectField";
 import SelectField from "./SelectField";
+import DateTimeField from "./DateTimeField";
 import Field from "./Field";
 
-export { BooleanField, StringField, DurationField, StringSliceField, IntegerField, MultiSelectField, SelectField, Field }
+export { BooleanField, StringField, DurationField, StringSliceField, IntegerField, MultiSelectField, SelectField, DateTimeField, Field }
