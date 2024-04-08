277c67d72f99d4b796a6628fa9aa2ddc9ea3c8db
Haoming Meng <41393704+haoming29@users.noreply.github.com>
2024-04-08 14:21:40 -0500

    Update Origin UI to use API for the new `Origin.Exports` scheme (#1042)
    * API for origin data export
    
    * Update UI to use the exports API
    
    * Fix uncaught merge conflict
    
    * Adapt UI for S3 storage backend
    
    * Add more precision to typing
    
    * Add more precision to typing
    
    ---------
    
    Co-authored-by: Cannon Lock <clock@wisc.edu>

--- web_ui/frontend/components/DataExportTable.tsx
+++ web_ui/frontend/components/DataExportTable.tsx
@@ -15,16 +15,30 @@ import {
 import React, {FunctionComponent, ReactElement, useEffect, useMemo, useRef, useState} from "react";
 import {Skeleton} from "@mui/material";
 
+interface Capabilities {
+    PublicReads: boolean;
+    Reads: boolean;
+    Writes: boolean;
+    Listings: boolean;
+    DirectReads: boolean;
+  }
 
+type ExportRes = { type: "s3", exports: S3ExportEntry[] } | { type: "posix", exports: PosixExportEntry[]};
 
-interface Record {
-    [key: string]: string | number | boolean | null
+interface ExportEntry {
+    federation_prefix: string;
+    capabilities: Capabilities;
 }
 
-interface ExportData extends Record {
-    "Type": string
-    "Local Path": string
-    "Namespace Prefix": string
+interface S3ExportEntry extends ExportEntry {
+    s3_access_keyfile: string;
+    s3_secret_keyfile: string;
+    s3_bucket: string;
+}
+
+interface PosixExportEntry extends ExportEntry {
+    storage_prefix: string;
+    sentinel_location: string;
 }
 
 export const TableCellOverflow: FunctionComponent<any> = ({ children, ...props }) => {
@@ -52,23 +66,23 @@ export const TableCellOverflow: FunctionComponent<any> = ({ children, ...props }
     )
 }
 
-export const RecordTable = ({ data }: { data: Record[] }): ReactElement  => {
+export const RecordTable = ({ data }: { data: ExportRes }): ReactElement  => {
     return (
         <TableContainer>
             <Table sx={{tableLayout: "fixed"}}>
                 <TableHead>
                     <TableRow>
-                        {Object.keys(data[0]).map((key, index) => (
-                            <TableCell key={index} sx={{width: index == 0 ? "20%" : "40%"}}>{key}</TableCell>
-                        ))}
+                        <TableCell sx={{width: "20%"}}>Type</TableCell>
+                        <TableCell sx={{width: "40%"}}>{data.type == "s3" ? "S3 Bucket Name" : "Local Path"}</TableCell>
+                        <TableCell sx={{width: "40%"}}>Federation Path</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {data.map((record, index) => (
-                        <TableRow key={index}>
-                            {Object.values(record).map((value, index) => (
-                                <TableCellOverflow key={index} sx={{width: index == 0 ? "20%" : "40%"}}>{value == null ? "NULL" : value}</TableCellOverflow>
-                            ))}
+                    {data?.exports.map((record, index) => (
+                        <TableRow key={record?.federation_prefix}>
+                            <TableCellOverflow key={record.federation_prefix} sx={{width: "20%"}}>{data.type == null ? "NULL" : data.type.toUpperCase()}</TableCellOverflow>
+                            <TableCellOverflow key={record.federation_prefix} sx={{width: "40%"}}>{data.type == "s3" ? ((record as S3ExportEntry).s3_bucket || "NULL") : ((record as PosixExportEntry)?.storage_prefix || "NULL")}</TableCellOverflow>
+                            <TableCellOverflow key={record.federation_prefix} sx={{width: "40%"}}>{record?.federation_prefix == null ? "NULL" : record?.federation_prefix}</TableCellOverflow>
                         </TableRow>
                     ))}
                 </TableBody>
@@ -79,21 +93,15 @@ export const RecordTable = ({ data }: { data: Record[] }): ReactElement  => {
 
 export const DataExportTable = ({boxProps}: {boxProps?: BoxProps}) => {
 
-    const [data, setData] = useState<ExportData[] | undefined>(undefined);
+    const [data, setData] = useState<ExportRes | undefined>(undefined);
     const [error, setError] = useState<string | undefined>(undefined);
 
 
     const getData = async () => {
-        let response = await fetch("/api/v1.0/config")
+        let response = await fetch("/api/v1.0/origin_ui/exports")
         if (response.ok) {
             const responseData = await response.json()
-
-            setData([{
-                "Type": "POSIX",
-                "Local Path": ["", undefined].includes(responseData?.Xrootd?.Mount?.Value) ? "NULL" : responseData?.Xrootd?.Mount?.Value,
-                "Namespace Prefix": ["", undefined].includes(responseData?.Origin?.NamespacePrefix?.Value) ? "NULL" : responseData?.Origin?.NamespacePrefix?.Value
-            }])
-
+            setData(responseData)
         } else {
             setError("Failed to fetch config, response status: " + response.status)
         }
