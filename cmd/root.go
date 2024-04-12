5e9b444e9135cfdd39e955adb5c75072da2d6bea
Haoming Meng <hmeng29@wisc.edu>
2024-04-12 19:04:38 +0000

    Refactor the code based on review comments

--- cmd/root.go
+++ cmd/root.go
@@ -133,7 +133,7 @@ func init() {
 	rootCmd.AddCommand(rootConfigCmd)
 	rootCmd.AddCommand(rootPluginCmd)
 	rootCmd.AddCommand(serveCmd)
-	rootCmd.AddCommand(keygenCmd)
+	rootCmd.AddCommand(generateCmd)
 	preferredPrefix := config.GetPreferredPrefix()
 	rootCmd.Use = strings.ToLower(preferredPrefix)
 
