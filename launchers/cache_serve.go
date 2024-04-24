ba584af28531db960754b6b308156fe3f6285ee1
Haoming Meng <hmeng29@wisc.edu>
2024-04-24 19:02:27 +0000

    Make the code testable and add tests

--- launchers/cache_serve.go
+++ launchers/cache_serve.go
@@ -24,9 +24,6 @@ import (
 	"context"
 	_ "embed"
 	"net/url"
-	"os"
-	"path"
-	"path/filepath"
 	"strconv"
 	"time"
 
@@ -54,19 +51,8 @@ func CacheServe(ctx context.Context, engine *gin.Engine, egrp *errgroup.Group, m
 		return nil, err
 	}
 
-	// Check for the sentinel file
-	if param.Cache_SentinelLocation.IsSet() {
-		sentinelPath := param.Cache_SentinelLocation.GetString()
-		dataLoc := param.Cache_DataLocation.GetString()
-		sentinelPath = path.Clean(sentinelPath)
-		if path.Base(sentinelPath) != sentinelPath {
-			return nil, errors.Errorf("invalid Cache.SentinelLocation path. File must not contain a directory. Got %s", sentinelPath)
-		}
-		fullPath := filepath.Join(dataLoc, sentinelPath)
-		_, err := os.Stat(fullPath)
-		if err != nil {
-			return nil, errors.Wrapf(err, "failed to open Cache.SentinelLocation %s. Directory check failed", fullPath)
-		}
+	if err := cache.CheckCacheSentinelLocation(); err != nil {
+		return nil, err
 	}
 
 	cache.RegisterCacheAPI(engine, ctx, egrp)
