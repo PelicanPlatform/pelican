b0138007303b5a2ef653432da36e5138402f2796
Cannon Lock <clock@wisc.edu>
2024-05-03 11:21:17 -0500

    Add a denied namespaces page
    - From here you can delete or approve previously denied namespace if you are an admin

---
+++ web_ui/frontend/components/Namespace/DeniedCard.tsx
@@ -0,0 +1,160 @@
+import React, {useMemo, useRef, useState} from "react";
+import {Authenticated, secureFetch} from "@/helpers/login";
+import {Avatar, Box, IconButton, Tooltip, Typography} from "@mui/material";
+import {Block, Check, Delete, Edit, Person} from "@mui/icons-material";
+import {Alert as AlertType, Alert, Namespace} from "@/components/Main";
+import InformationDropdown from "./InformationDropdown";
+import {getServerType, NamespaceIcon} from "@/components/Namespace/index";
+import {User} from "@/index";
+import AlertPortal from "@/components/AlertPortal";
+import { useSWRConfig } from 'swr'
+
+export interface DeniedCardProps {
+    namespace: Namespace;
+    onUpdate: () => void;
+    onAlert: (alert: Alert) => void;
+    authenticated?: User
+}
+
+const deleteNamespace = async (id: number) => {
+    const response = await secureFetch(`/api/v1.0/registry_ui/namespaces/${id}`, {
+        method: "DELETE"
+    })
+
+    if (!response.ok){
+        let alertMessage;
+        try {
+            let data = await response.json()
+            if (data?.message) {
+                alertMessage = data?.message
+            }
+            alertMessage = "Details not provided"
+        } catch (e) {
+            alertMessage = e
+        }
+
+        throw new Error("Failed to delete namespace: " + alertMessage)
+    }
+}
+
+const approveNamespace = async (id: number) => {
+    const response = await secureFetch(`/api/v1.0/registry_ui/namespaces/${id}/approve`, {
+        method: "PATCH"
+    })
+
+    if (!response.ok){
+        let alertMessage;
+        try {
+            let data = await response.json()
+            if (data?.message) {
+                alertMessage = data?.message
+            }
+            alertMessage = "Details not provided"
+        } catch (e) {
+            alertMessage = e
+        }
+
+        throw new Error("Failed to approve namespace: " + alertMessage)
+    }
+}
+
+export const DeniedCard = ({
+                                namespace,
+                                authenticated
+                            }: DeniedCardProps) => {
+
+    const ref = useRef<HTMLDivElement>(null);
+    const [transition, setTransition] = useState<boolean>(false);
+    const [alert, setAlert] = useState<AlertType | undefined>(undefined)
+
+    const { mutate } = useSWRConfig()
+
+    return (
+        <>
+            <Box>
+                <Box sx={{
+                    cursor: "pointer",
+                    display: "flex",
+                    width: "100%",
+                    justifyContent: "space-between",
+                    border: "solid #ececec 1px",
+                    borderRadius: transition ? "10px 10px 0px 0px" : 2,
+                    "&:hover": {
+                        bgcolor: "#ececec"
+                    },
+                    p: 1
+                }}
+                     bgcolor={"secondary"}
+                     onClick={() => setTransition(!transition)}
+                >
+                    <Box my={"auto"} ml={1} display={"flex"} flexDirection={"row"}>
+                        <NamespaceIcon serverType={namespace.type} />
+                        <Typography sx={{pt: "2px"}}>{namespace.prefix}</Typography>
+                    </Box>
+                    <Box display={"flex"}>
+                        <Box my={"auto"} display={"flex"} flexDirection={"row"}>
+                            { authenticated !== undefined && authenticated.user == namespace.admin_metadata.user_id &&
+                                <Box sx={{borderRight: "solid 1px #ececec", mr: 1}}>
+                                    <Tooltip title={"Created By You"}>
+                                        <Avatar sx={{height: "40px", width: "40px", my: "auto", mr:2}}>
+                                            <Person/>
+                                        </Avatar>
+                                    </Tooltip>
+                                </Box>
+                            }
+                            { authenticated?.role == "admin" &&
+                                <>
+                                    <Tooltip title={"Delete Registration"}>
+                                        <IconButton
+                                            sx={{bgcolor: "#ff00001a", mx: 1}}
+                                            color={"error"}
+                                            onClick={async (e) => {
+                                                e.stopPropagation()
+                                                try {
+                                                    await deleteNamespace(namespace.id)
+                                                    await mutate("getNamespaces")
+                                                } catch (e) {
+                                                    if(e instanceof Error){
+                                                        setAlert({severity: "error", message: e.message})
+                                                    }
+                                                }
+                                            }}
+                                        >
+                                            <Delete/>
+                                        </IconButton>
+                                    </Tooltip>
+                                    <Tooltip title={"Approve Registration"}>
+                                        <IconButton
+                                            sx={{bgcolor: "#2e7d3224", mx: 1}}
+                                            color={"success"}
+                                            onClick={async (e) => {
+                                                e.stopPropagation()
+                                                try {
+                                                    await approveNamespace(namespace.id)
+                                                    await mutate("getNamespaces")
+                                                } catch (e) {
+                                                    if(e instanceof Error){
+                                                        setAlert({severity: "error", message: e.message})
+                                                    }
+                                                }
+                                            }}
+                                        >
+                                            <Check/>
+                                        </IconButton>
+                                    </Tooltip>
+                                </>
+                            }
+                        </Box>
+                    </Box>
+                </Box>
+                <Box ref={ref}>
+                    <InformationDropdown adminMetadata={namespace.admin_metadata} transition={transition} parentRef={ref}/>
+                </Box>
+            </Box>
+            <AlertPortal alert={alert} onClose={() => setAlert(undefined)} />
+        </>
+
+    )
+}
+
+export default DeniedCard;
