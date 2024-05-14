694b3524e82fef62e7aaee9acf2e85517ae63e81
Haoming Meng <hmeng29@wisc.edu>
2024-05-14 20:07:20 +0000

    Use externalWebUrl as the token issuer for self-tests

--- origin/self_monitor.go
+++ origin/self_monitor.go
@@ -22,21 +22,18 @@ import (
 	"context"
 	"time"
 
+	log "github.com/sirupsen/logrus"
+
 	"github.com/pelicanplatform/pelican/config"
 	"github.com/pelicanplatform/pelican/metrics"
 	"github.com/pelicanplatform/pelican/param"
 	"github.com/pelicanplatform/pelican/server_utils"
-	log "github.com/sirupsen/logrus"
 )
 
 func doSelfMonitor(ctx context.Context) {
 	log.Debug("Starting a new self-test monitoring cycle")
 	fileTests := server_utils.TestFileTransferImpl{}
-	issuerUrl, err := config.GetServerIssuerURL()
-	if err != nil {
-		log.Warningln("Self-test monitoring cycle failed due to lack of issuer URL: ", err)
-		metrics.SetComponentHealthStatus(metrics.OriginCache_XRootD, metrics.StatusCritical, "Self-test monitoring cycle due to lack of issuer URL: "+err.Error())
-	}
+	issuerUrl := param.Server_ExternalWebUrl.GetString()
 	ok, err := fileTests.RunTests(ctx, param.Origin_Url.GetString(), config.GetServerAudience(), issuerUrl, server_utils.OriginSelfFileTest)
 	if ok && err == nil {
 		log.Debugln("Self-test monitoring cycle succeeded at", time.Now().Format(time.UnixDate))
