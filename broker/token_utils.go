d4f5ed2e34b6d068bac9c2e99ee531fd30083825
Haoming Meng <hmeng29@wisc.edu>
2024-05-30 16:28:55 +0000

    Fix test failure

--- broker/token_utils.go
+++ broker/token_utils.go
@@ -147,7 +147,7 @@ func getCacheHostnameFromToken(token []byte) (hostname string, err error) {
 	if err != nil {
 		return
 	}
-	hostname, hasPrefix := strings.CutPrefix(iss, expectedPrefix+"/")
+	hostname, hasPrefix := strings.CutPrefix(iss, expectedPrefix)
 	if !hasPrefix {
 		err = errors.Errorf("Token issuer %s doesn't start with expected registry issuer %s", iss, expectedPrefix)
 		return
