a287395b44d2d5cddc3918373b4d777f335116ce
Justin Hiemstra <jhiemstra@wisc.edu>
2024-06-12 16:59:14 +0000

    Recursively clone submodules for xrootd-s3-http

--- github_scripts/osx_install.sh
+++ github_scripts/osx_install.sh
@@ -37,7 +37,7 @@ sudo mkdir -p /etc/xrootd/client.plugins.d/
 sudo cp release_dir/etc/xrootd/client.plugins.d/pelican-plugin.conf /etc/xrootd/client.plugins.d/
 popd
 
-git clone --branch v0.1.3 https://github.com/PelicanPlatform/xrootd-s3-http.git
+git clone --recursive-submodules --branch v0.1.3 https://github.com/PelicanPlatform/xrootd-s3-http.git
 pushd xrootd-s3-http
 git checkout v0.1.3
 mkdir build
