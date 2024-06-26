05ace396da119f808112a6713ed3a17fc377a095
Cannon Lock <clock@wisc.edu>
2024-06-26 15:11:59 -0500

    GZIP web responses
    8.2mb to 1.8mb

--- web_ui/ui.go
+++ web_ui/ui.go
@@ -551,7 +551,7 @@ func GetEngine() (*gin.Engine, error) {
 	gin.SetMode(gin.ReleaseMode)
 	engine := gin.New()
 	engine.Use(gin.Recovery())
-	engine.Use(gzip.Gzip(gzip.DefaultCompression))
+	engine.Use(gzip.Gzip(gzip.DefaultCompression, gzip.WithExcludedPaths([]string{"/api/v1.0/prometheus"})))
 	webLogger := log.WithFields(log.Fields{"daemon": "gin"})
 	engine.Use(func(ctx *gin.Context) {
 		startTime := time.Now()
