e27231e022a53188e5809fac0ec697d70e31810b
Howard Zhong <hzhong45@wisc.edu>
2024-12-18 18:52:58 +0000

    fix linter and go build tests

--- param/parameters.go
+++ param/parameters.go
@@ -56,7 +56,6 @@ func GetDeprecated() map[string][]string {
         "Director.EnableStat": {"Director.CheckOriginPresence"},
         "DisableHttpProxy": {"Client.DisableHttpProxy"},
         "DisableProxyFallback": {"Client.DisableProxyFallback"},
-        "IssuerKey": {"IssuerKeysDirectory"},
         "MinimumDownloadSpeed": {"Client.MinimumDownloadSpeed"},
         "Origin.EnableDirListing": {"Origin.EnableListings"},
         "Origin.EnableFallbackRead": {"Origin.EnableDirectReads"},
