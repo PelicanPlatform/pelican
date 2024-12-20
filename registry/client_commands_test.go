752ab32b74678464261b0bf87063b19a1030e730
Howard Zhong <hzhong45@wisc.edu>
2024-12-20 07:13:17 +0000

    10th attempt to fix timeout

--- registry/client_commands_test.go
+++ registry/client_commands_test.go
@@ -302,6 +302,111 @@ func TestMultiPubKeysRegisteredOnNamespace(t *testing.T) {
 	require.Equal(t, expectedKids, actualKids)
 }
 
+func TestRegistryKeyChainingOSDF(t *testing.T) {
+	config.ResetIssuerJWKPtr()
+	config.ResetIssuerPrivateKeys()
+	ctx, cancel, egrp := test_utils.TestContext(context.Background(), t)
+	t.Cleanup(func() {
+		func() { require.NoError(t, egrp.Wait()) }()
+		cancel()
+		config.ResetIssuerJWKPtr()
+		config.ResetIssuerPrivateKeys()
+		server_utils.ResetTestState()
+	})
+
+	server_utils.ResetTestState()
+	_, err := config.SetPreferredPrefix(config.OsdfPrefix)
+	assert.NoError(t, err)
+	viper.Set("Federation.DirectorUrl", "https://osdf-director.osg-htc.org")
+	viper.Set("Federation.RegistryUrl", "https://osdf-registry.osg-htc.org")
+	viper.Set("Federation.JwkUrl", "https://osg-htc.org/osdf/public_signing_key.jwks")
+	viper.Set("Federation.BrokerUrl", "https://osdf-director.osg-htc.org")
+
+	// On by default, but just to make things explicit
+	viper.Set("Registry.RequireKeyChaining", true)
+
+	registrySvr := registryMockup(ctx, t, "OSDFkeychaining")
+	topoSvr := topologyMockup(t, []string{"/topo/foo"})
+	viper.Set("Federation.TopologyNamespaceURL", topoSvr.URL)
+	err = migrateTopologyTestTable()
+	require.NoError(t, err)
+	err = PopulateTopology(ctx)
+	require.NoError(t, err)
+
+	defer func() {
+		err := ShutdownRegistryDB()
+		assert.NoError(t, err)
+		registrySvr.CloseClientConnections()
+		registrySvr.Close()
+		topoSvr.CloseClientConnections()
+		topoSvr.Close()
+	}()
+
+	privKey, err := config.GetIssuerPrivateJWK()
+	require.NoError(t, err)
+	_, err = config.GetIssuerPublicJWKS()
+	require.NoError(t, err)
+
+	// Start by registering /foo/bar with the default key
+	err = NamespaceRegister(privKey, registrySvr.URL+"/api/v1.0/registry", "", "/foo/bar", "")
+	require.NoError(t, err)
+
+	// Perform one test with a subspace and the same key -- should succeed
+	err = NamespaceRegister(privKey, registrySvr.URL+"/api/v1.0/registry", "", "/foo/bar/test", "")
+	require.NoError(t, err)
+
+	// If the namespace is a subspace from the topology and is registered without the identity
+	// we deny it
+	err = NamespaceRegister(privKey, registrySvr.URL+"/api/v1.0/registry", "", "/topo/foo/bar", "")
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), "A superspace or subspace of this namespace /topo/foo/bar already exists in the OSDF topology: /topo/foo. To register a Pelican equivalence, you need to present your identity.")
+
+	err = NamespaceRegister(privKey, registrySvr.URL+"/api/v1.0/registry", "", "/topo/foo", "")
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), "A superspace or subspace of this namespace /topo/foo already exists in the OSDF topology: /topo/foo. To register a Pelican equivalence, you need to present your identity.")
+
+	// Now we create a new key and try to use it to register a super/sub space. These shouldn't succeed
+	viper.Set("IssuerKeysDirectory", t.TempDir()+"/keychaining2")
+	viper.Set("ConfigDir", t.TempDir())
+	config.InitConfig()
+	err = config.InitServer(ctx, server_structs.RegistryType)
+	require.NoError(t, err)
+
+	privKey, err = config.GetIssuerPrivateJWK()
+	require.NoError(t, err)
+	_, err = config.GetIssuerPublicJWKS()
+	require.NoError(t, err)
+
+	err = NamespaceRegister(privKey, registrySvr.URL+"/api/v1.0/registry", "", "/foo/bar/baz", "")
+	require.ErrorContains(t, err, "Cannot register a namespace that is suffixed or prefixed")
+
+	err = NamespaceRegister(privKey, registrySvr.URL+"/api/v1.0/registry", "", "/foo", "")
+	require.ErrorContains(t, err, "Cannot register a namespace that is suffixed or prefixed")
+
+	// Make sure we can register things similar but distinct in prefix and suffix
+	err = NamespaceRegister(privKey, registrySvr.URL+"/api/v1.0/registry", "", "/fo", "")
+	require.NoError(t, err)
+	err = NamespaceRegister(privKey, registrySvr.URL+"/api/v1.0/registry", "", "/foo/barz", "")
+	require.NoError(t, err)
+
+	// Now turn off token chaining and retry -- no errors should occur
+	viper.Set("Registry.RequireKeyChaining", false)
+	err = NamespaceRegister(privKey, registrySvr.URL+"/api/v1.0/registry", "", "/foo/bar/baz", "")
+	require.NoError(t, err)
+
+	err = NamespaceRegister(privKey, registrySvr.URL+"/api/v1.0/registry", "", "/foo", "")
+	require.NoError(t, err)
+
+	// However, topology check should be independent of key chaining check
+	err = NamespaceRegister(privKey, registrySvr.URL+"/api/v1.0/registry", "", "/topo", "")
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), "A superspace or subspace of this namespace /topo already exists in the OSDF topology: /topo/foo. To register a Pelican equivalence, you need to present your identity.")
+
+	_, err = config.SetPreferredPrefix(config.PelicanPrefix)
+	assert.NoError(t, err)
+	server_utils.ResetTestState()
+}
+
 func TestRegistryKeyChaining(t *testing.T) {
 	ctx, cancel, egrp := test_utils.TestContext(context.Background(), t)
 	defer func() { require.NoError(t, egrp.Wait()) }()
@@ -333,15 +438,15 @@ func TestRegistryKeyChaining(t *testing.T) {
 	require.NoError(t, err)
 
 	// Now we create a new key and try to use it to register a super/sub space. These shouldn't succeed
-	viper.Set("IssuerKeysDirectory", t.TempDir()+"/keychaining")
+	viper.Set("IssuerKeysDirectory", t.TempDir()+"/keychaining2")
 	viper.Set("ConfigDir", t.TempDir())
 	config.InitConfig()
 	err = config.InitServer(ctx, server_structs.RegistryType)
 	require.NoError(t, err)
 
-	_, err = config.GetIssuerPublicJWKS()
-	require.NoError(t, err)
 	privKey, err = config.GetIssuerPrivateJWK()
+	require.NoError(t, err)
+	_, err = config.GetIssuerPublicJWKS()
 	require.NoError(t, err)
 
 	err = NamespaceRegister(privKey, registrySvr.URL+"/api/v1.0/registry", "", "/foo/bar/baz", "")
