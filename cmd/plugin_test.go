ec31c71fc84f82427703039dd2739c21803ef3d5
Brian Bockelman <bbockelman@morgridge.org>
2025-02-15 19:13:51 -0600

    Simplify the default case of the federation test
    With this, an empty configuration string indicates to use the default.

--- cmd/plugin_test.go
+++ cmd/plugin_test.go
@@ -566,7 +566,7 @@ func TestPluginDirectRead(t *testing.T) {
 	viper.Set("Origin.StoragePrefix", "/<SOMETHING THAT WILL BE OVERRIDDEN>")
 	viper.Set("Origin.EnablePublicReads", true)
 	viper.Set("Origin.EnableDirectReads", true)
-	fed := fed_test_utils.NewFedTest(t, "")
+	fed := fed_test_utils.NewFedTest(t, " ")
 	host := param.Server_Hostname.GetString() + ":" + strconv.Itoa(param.Server_WebPort.GetInt())
 
 	log.Debugln("Will create origin file at", fed.Exports[0].StoragePrefix)
@@ -852,7 +852,7 @@ func TestPluginRecursiveDownload(t *testing.T) {
 	viper.Set("Origin.FederationPrefix", "/test")
 	viper.Set("Origin.StoragePrefix", "/<THIS WILL BE OVERRIDDEN>")
 	viper.Set("Origin.EnablePublicReads", true)
-	fed := fed_test_utils.NewFedTest(t, "")
+	fed := fed_test_utils.NewFedTest(t, " ")
 	host := param.Server_Hostname.GetString() + ":" + strconv.Itoa(param.Server_WebPort.GetInt())
 
 	// Drop the testFileContent into the origin directory
