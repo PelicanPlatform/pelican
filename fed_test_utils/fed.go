78d411eab6a2eca5771c79c470b059d0b19bd275
Patrick <patrick.brophy2@gmail.com>
2025-02-17 17:01:17 +0000

    Configure cache db

--- fed_test_utils/fed.go
+++ fed_test_utils/fed.go
@@ -168,7 +168,7 @@ func NewFedTest(t *testing.T, originConfig string) (ft *FedTest) {
 	viper.Set(param.Server_EnableUI.GetName(), false)
 	viper.Set(param.Server_WebPort.GetName(), 0)
 	viper.Set(param.LocalCache_RunLocation.GetName(), filepath.Join(tmpPath, "local-cache"))
-	viper.Set("Server.DbLocation", filepath.Join(t.TempDir(), "server.sqlite"))
+	viper.Set(param.Cache_DbLocation.GetString(), filepath.Join(t.TempDir(), "server.sqlite"))
 
 	// Set the Director's start time to 6 minutes ago. This prevents it from sending an HTTP 429 for
 	// unknown prefixes.
