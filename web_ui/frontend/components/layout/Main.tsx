e049c531d2617c3730846be61824891e438be1f2
Cannon Lock <clock@wisc.edu>
2025-03-20 10:41:00 -0500

    Display MaxMind Attribution on Director Interfaces

--- web_ui/frontend/components/layout/Main.tsx
+++ web_ui/frontend/components/layout/Main.tsx
@@ -1,18 +1,35 @@
-import { Box } from '@mui/material';
+import { Box, Link } from '@mui/material';
 import { ReactNode } from 'react';
 
-export const Main = ({ children }: { children: ReactNode }) => {
+interface MainProps {
+  children: ReactNode
+  displayMaxmindAttribution?: boolean
+}
+
+export const Main = ({ children, displayMaxmindAttribution = false }: MainProps) => {
   return (
     <Box
-      component={'main'}
-      pl={0}
-      ml={{ xs: 0, md: '72px' }}
-      display={'flex'}
-      minHeight={'100vh'}
+      display={"flex"}
+      flexDirection={"column"}
       flexGrow={1}
-      zIndex={1}
     >
-      {children}
+      <Box
+        component={'main'}
+        pl={0}
+        ml={{ xs: 0, md: '72px' }}
+        display={'flex'}
+        minHeight={'100vh'}
+        flexGrow={1}
+        zIndex={1}
+      >
+        {children}
+      </Box>
+      {
+        displayMaxmindAttribution &&
+        <Box p={2} display={"flex"}>
+          <Link mx={"auto"} href='https://www.maxmind.com/en/home'>Free IP geolocation by MaxMind</Link>
+        </Box>
+      }
     </Box>
   );
 };
