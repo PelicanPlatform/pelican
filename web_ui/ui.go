f0636f862c3005d619eaea40df7d8e08cf37763c
Cannon Lock <clock@wisc.edu>
2025-03-21 16:01:16 -0500

    Align token endpoints with REST API best practices
    Just nice to keep things standard with other applications. An okay overview can be found here: https://stackoverflow.blog/2020/03/02/best-practices-for-rest-api-design/#h-use-nouns-instead-of-verbs-in-endpoint-paths
    
    Big things here are with the way things are written I have to remember the endpoint for each item. If you align with REST best practices all I have to know is `/tokens` and I can do basically all the actions I would ever want using HTTP request methods.

--- web_ui/ui.go
+++ web_ui/ui.go
@@ -572,10 +572,9 @@ func configureCommonEndpoints(engine *gin.Engine) error {
 	engine.GET("/api/v1.0/health", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("Web Engine Running. Time: %s", time.Now().String())})
 	})
-	engine.POST("/api/v1.0/createApiToken", AuthHandler, AdminAuthHandler, createApiToken)
-	engine.DELETE("/api/v1.0/deleteApiToken/:id", AuthHandler, AdminAuthHandler, deleteApiToken)
-	engine.GET("/api/v1.0/listApiTokens", AuthHandler, AdminAuthHandler, listApiTokens)
-	engine.GET("/api/v1.0/version", getVersionHandler)
+	engine.POST("/api/v1.0/tokens", createApiToken)
+	engine.DELETE("/api/v1.0/tokens/:id", deleteApiToken)
+	engine.GET("/api/v1.0/tokens", listApiTokens)
 	return nil
 }
 
