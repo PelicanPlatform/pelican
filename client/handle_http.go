ef11005978f1868925e2f2d7851641527277b46d
Justin Hiemstra <jhiemstra@wisc.edu>
2025-04-04 20:17:41 +0000

    Sanity check received size and on-disk size for client GETs
    When the client requests an object, grab determines the expected download
    size by inspecting the `Content-Length` header. Previously we didn't perform
    any checks against this value to make sure the number of bytes we receive
    actually match the number of expected bytes -- we only used status codes
    to determine success.
    
    With this diff, we do two sanity checks. The first inspects the number of bytes
    that ran through the grab client's writer, and the second actually looks at
    on-disk metadata to make sure the correct number of bytes were persisted.
    
    If the grab sanity check passes but there's an error with the stat, I don't
    treat that as an error. I could easily be convinced that it _should_ be an
    error, but I don't know enough about how stat is supposed to perform on all
    systems to make that call at this point.

--- client/handle_http.go
+++ client/handle_http.go
@@ -2232,6 +2232,30 @@ Loop:
 			resp.HTTPResponse.StatusCode, resp.Err().Error())}
 	}
 
+	// By now, we think the download succeeded. If we know how large the file was supposed to
+	// be based on the Content-Length header, we can check that a) the grab client witnessed
+	// the correct number of bytes and b) the file on disk is the correct size. If totalSize is
+	// <= 0, it indicates we don't know how large the transfer was supposed to be in the first
+	// place.
+	if totalSize > 0 {
+		// Check size of received bytes
+		if downloaded != totalSize {
+			log.WithFields(fields).Debugf("Download completed but received size '%db' does not match expected size '%db'", downloaded, totalSize)
+			err = errors.Errorf("download completed but received size '%db' does not match expected size '%db'", downloaded, totalSize)
+			return
+		}
+		// Check size of file on disk
+		fi, statErr := os.Stat(dest)
+		if statErr == nil { // Don't treat stat errors as a failure -- we've already passed one sanity check so we should move on.
+			sizeOnDisk := fi.Size()
+			if sizeOnDisk != totalSize {
+				log.WithFields(fields).Debugf("File size on disk '%db' does not match expected size '%db'", sizeOnDisk, totalSize)
+				err = errors.Errorf("file size on disk '%db' does not match expected size '%db'", sizeOnDisk, totalSize)
+				return
+			}
+		}
+	}
+
 	if unpacker != nil {
 		unpacker.Close()
 		if err = unpacker.Error(); err != nil {
