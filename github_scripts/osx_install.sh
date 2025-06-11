99eb78dbbe546c52722906b9aa271bfdd3bae15d
Justin Hiemstra <jhiemstra@wisc.edu>
2025-06-11 13:53:36 +0000

    Bump containerized XRootD version to 5.8.3-1.1

--- github_scripts/osx_install.sh
+++ github_scripts/osx_install.sh
@@ -46,7 +46,7 @@ popd
 # Add patches to xrootd source code if needed
 git clone https://github.com/PelicanPlatform/xrootd.git
 pushd xrootd
-git checkout v5.8.2-pelican
+git checkout v5.8.3-pelican
 mkdir xrootd_build
 cd xrootd_build
 cmake .. -GNinja
