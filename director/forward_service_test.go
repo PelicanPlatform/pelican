a1a0432c8e8612f8e8e5b8d354749a9552647c24
Patrick <patrick.brophy2@gmail.com>
2025-06-23 16:08:45 +0000

    Add forwardService test

--- director/forward_service_test.go
+++ director/forward_service_test.go
@@ -84,3 +84,76 @@ func TestForwardService(t *testing.T) {
 	assert.False(t, fwd.Now.IsZero())
 	assert.Equal(t, ad, fwd.ServiceAd)
 }
+
+// TestForwardServiceAd tests that we forward the service ad to the correct director
+// We have two directors, each with different advertise urls
+// We have a service ad that should be only forwarded to the director that is not itself
+func TestForwardServiceAd(t *testing.T) {
+	viper.Set(param.Server_ExternalWebUrl.GetName(), "http://director1.com")
+	defer viper.Set(param.Server_ExternalWebUrl.GetName(), "")
+
+	// Reset the cache
+	directorAds.DeleteAll()
+
+	ch1 := make(chan *forwardAdInfo, 1)
+	dir1 := &directorInfo{
+		ad: &server_structs.DirectorAd{
+			AdvertiseUrl: "http://director-ad-url-1",
+			ServerBaseAd: server_structs.ServerBaseAd{
+				Name:         "dir1",
+				InstanceID:   "inst-id-1",
+				StartTime:    12345,
+				GenerationID: 1,
+				Version:      "v1",
+			},
+		},
+		forwardAdChan: ch1,
+	}
+
+	ch2 := make(chan *forwardAdInfo, 1)
+	dir2 := &directorInfo{
+		ad: &server_structs.DirectorAd{
+			AdvertiseUrl: "http://director-ad-url-2",
+			ServerBaseAd: server_structs.ServerBaseAd{
+				Name:         "dir2",
+				InstanceID:   "inst-id-2",
+				StartTime:    12345,
+				GenerationID: 1,
+				Version:      "v1",
+			},
+		},
+		forwardAdChan: ch2,
+	}
+
+	directorAds.Set("dir1", dir1, 15*time.Minute)
+	directorAds.Set("dir2", dir2, 15*time.Minute)
+
+	// Build a fake service ad
+	svcAd := &server_structs.OriginAdvertiseV2{
+		ServerBaseAd: server_structs.ServerBaseAd{
+			Name:         "svc-name",
+			InstanceID:   "inst-id",
+			StartTime:    12345,
+			GenerationID: 1,
+			Version:      "v1",
+			Expiration:   time.Unix(67890, 0).UTC(),
+		},
+	}
+
+	ctx := context.Background()
+	forwardServiceAd(ctx, svcAd, server_structs.OriginType, dir1.ad)
+
+	// We should have received an ad on channel 2 but not channel 1
+	select {
+	case adInfo := <-ch1:
+		assert.Fail(t, "Received an ad on channel 1 but it should have been skipped", adInfo.serverBase)
+	default:
+	}
+
+	select {
+	case adInfo := <-ch2: // We should have received an ad on channel 2
+		assert.Equal(t, svcAd.ServerBaseAd, adInfo.serverBase)
+	default:
+		assert.Fail(t, "Failed to receive an ad on channel 2")
+	}
+}
