fa829c29e9b732c06874db2b4ba036da9e3ea43d
Howard Zhong <hzhong45@wisc.edu>
2025-07-24 05:41:51 +0000

    Fix "Unable to find scitoken config file" errors during Pelican startup
    - set isFirstRun=true for initial scitoken config file write (writeScitokensConfiguration)
    - If the drop privileges feature is enabled, the first run is by the root user, and the subsequent runs are by the unprivileged pelican user via the xrdhttp-pelican pluginas the unprivileged pelican user via the xrdhttp-pelican plugin.
    
    (cherry picked from commit 9a8a5f263a0f749cf57b7302e055660d32d9c8d6)

--- xrootd/xrootd_config.go
+++ xrootd/xrootd_config.go
@@ -273,7 +273,8 @@ func CheckOriginXrootdEnv(exportPath string, server server_structs.XRootDServer,
 	}
 	// If the scitokens.cfg does not exist, create one
 	if _, ok := server.(*origin.OriginServer); ok {
-		err = WriteOriginScitokensConfig()
+		// This is the first time we write the scitokens.cfg file, so isFirstRun = true
+		err = WriteOriginScitokensConfig(true)
 		if err != nil {
 			return err
 		}
@@ -393,7 +394,7 @@ func CheckCacheXrootdEnv(server server_structs.XRootDServer, uid int, gid int) e
 	}
 
 	if cacheServer, ok := server.(*cache.CacheServer); ok {
-		err := WriteCacheScitokensConfig(cacheServer.GetNamespaceAds())
+		err := WriteCacheScitokensConfig(cacheServer.GetNamespaceAds(), true)
 		if err != nil {
 			return errors.Wrap(err, "Failed to create scitokens configuration for the cache")
 		}
