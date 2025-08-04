e22dc75a751e571a858ee3a33861420339918755
Patrick Brophy <patrick.brophy2@gmail.com>
2025-08-04 18:52:11 +0000

    Adjust error conditions
    (cherry picked from commit 5cb8a6d273802173127156b0756aebbe685959c2)

--- client/handle_http.go
+++ client/handle_http.go
@@ -2289,17 +2289,14 @@ func downloadObject(transfer *transferFile) (transferResults TransferResults, er
 					if !bytes.Equal(checksumInfo.Value, computedValue) {
 						mismatchErr := &ChecksumMismatchError{
 							Info: ChecksumInfo{
-								Algorithm: checksumInfo.Algorithm,
+								Algorithm: checksum,
 								Value:     computedValue,
 							},
 							ServerValue: checksumInfo.Value,
 						}
-						if transfer.requireChecksum {
-							transferResults.Error = mismatchErr
-							log.WithFields(fields).Errorln(transferResults.Error)
-						} else {
-							log.WithFields(fields).Warnln(mismatchErr.Error() + " (not required, continuing)")
-						}
+						transferResults.Error = mismatchErr
+						log.WithFields(fields).Errorln(transferResults.Error)
+						break
 					} else {
 						successCtr++
 						log.WithFields(fields).Debugf("Checksum %s matches: %s",
@@ -3330,13 +3327,7 @@ Loop:
 			log.Errorln("Error fetching checksum:", err)
 			if transfer.requireChecksum {
 				transferResult.Error = errors.New("checksum is required but endpoint was not able to provide it")
-				attempt.Error = &ChecksumMismatchError{
-					Info: ChecksumInfo{
-						Algorithm: transfer.requestedChecksums[0],
-						Value:     hashes[0].(hash.Hash).Sum(nil),
-					},
-					ServerValue: nil,
-				}
+				attempt.Error = err
 			} else {
 				log.Warnln("Checksum is not required, but endpoint was not able to provide it. Continuing without verification.")
 			}
@@ -3365,21 +3356,15 @@ Loop:
 				if checksumInfo.Algorithm == checksum {
 					found = true
 					if !bytes.Equal(checksumInfo.Value, computedValue) {
-						mismatchErr := &ChecksumMismatchError{
+						transferResult.Error = &ChecksumMismatchError{
 							Info: ChecksumInfo{
 								Algorithm: checksum,
 								Value:     computedValue,
 							},
 							ServerValue: checksumInfo.Value,
 						}
-						if transfer.requireChecksum {
-							transferResult.Error = mismatchErr
-							log.WithFields(fields).Errorln(transferResult.Error)
-							attempt.Error = transferResult.Error
-							break
-						} else {
-							log.WithFields(fields).Warnln(mismatchErr.Error() + " (not required, continuing)")
-						}
+						log.WithFields(fields).Errorln(transferResult.Error)
+						break
 					} else {
 						successCtr++
 						log.WithFields(fields).Debugf("Checksum %s matches: %s",
