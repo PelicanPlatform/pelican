ce5ef4fe2f54bf55d24f60aebecb6399ed68f87b
Howard Zhong <hzhong45@wisc.edu>
2025-08-06 21:04:14 +0000

    Align json output fields with existing APIs - Hide the fields we don't want to include in the json

--- server_structs/registry.go
+++ server_structs/registry.go
@@ -212,16 +212,16 @@ type ServerNamespace struct {
 	IsOrigin  bool      `json:"is_origin"`
 	IsCache   bool      `json:"is_cache"`
 	Note      string    `json:"note"`
-	CreatedAt time.Time `json:"created_at" post:"exclude"`
-	UpdatedAt time.Time `json:"updated_at" post:"exclude"`
+	CreatedAt time.Time `json:"-" post:"exclude"`
+	UpdatedAt time.Time `json:"-" post:"exclude"`
 
 	// Namespace fields (ID renamed to NsID to avoid conflict)
-	NsID          int                    `json:"ns_id" post:"exclude"`
+	NsID          int                    `json:"-" post:"exclude"`
 	Prefix        string                 `json:"prefix"`
-	Pubkey        string                 `json:"pubkey"`
+	Pubkey        string                 `json:"-"`
 	Identity      string                 `json:"identity" post:"exclude"`
 	AdminMetadata AdminMetadata          `json:"admin_metadata" gorm:"serializer:json"`
-	CustomFields  map[string]interface{} `json:"custom_fields" gorm:"serializer:json"`
+	CustomFields  map[string]interface{} `json:"-" gorm:"serializer:json"`
 }
 
 // BeforeCreate GORM hook to auto-generate Server ID
