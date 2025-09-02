0d920dab19fd61801a4fd6a88284ac46d98020d6
Howard Zhong <hzhong45@wisc.edu>
2025-09-02 22:57:57 +0000

    Use jwk.Equal() instead of JSON string comparison for super/sub spaces key matching
    This method properly compares the cryptographic content rather than string representation, making it consistent with other parts of the codebase like:
    * compareJwks() 
    * checkNamespaceExistsHandler()
    
    Also add extra debugging logs

--- registry/registry.go
+++ registry/registry.go
@@ -130,6 +130,7 @@ func (e *duplicateServerIdError) Error() string {
 }
 
 func matchKeys(incomingKey jwk.Key, registeredNamespaces []string) (bool, error) {
+	log.Debugf("Matching the incoming key to the registered namespaces for namespace %v", registeredNamespaces)
 	// If this is the case, we want to make sure that at least one of the superspaces has the
 	// same registration key as the incoming. This guarantees the owner of the superspace is
 	// permitting the action (assuming their keys haven't been stolen!)
@@ -154,11 +155,18 @@ func matchKeys(incomingKey jwk.Key, registeredNamespaces []string) (bool, error)
 			if err != nil {
 				return false, errors.Wrap(err, "failed to marshal the incoming key into JSON")
 			}
+			log.Debugf("Matching key for namespace %s\n", ns)
+			log.Debugf("key in Registry DB: %s\n", string(registeredKeyBuf))
+			log.Debugf("key in incoming request: %s\n", string(incomingKeyBuf))
 
-			if string(registeredKeyBuf) == string(incomingKeyBuf) {
+			// Use jwk.Equal for reliable key comparison
+			if jwk.Equal(registeredKey, incomingKey) {
 				foundMatch = true
 				break
 			}
+		}
+		if foundMatch {
+			break
 		}
 	}
 
