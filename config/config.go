55720a7815fac903e2c9afac7a3e58a96df2f55a
Patrick Brophy <patrick.brophy2@gmail.com>
2025-09-16 22:08:44 +0000

    Fixed default to be 60s NOT 60ns

--- config/config.go
+++ config/config.go
@@ -1044,7 +1044,7 @@ func SetServerDefaults(v *viper.Viper) error {
 	v.SetDefault(param.Xrootd_Authfile.GetName(), filepath.Join(configDir, "xrootd", "authfile"))
 	v.SetDefault(param.Xrootd_MacaroonsKeyFile.GetName(), filepath.Join(configDir, "macaroons-secret"))
 	v.SetDefault(param.Xrootd_ShutdownTimeout.GetName(), 1*time.Minute)
-	v.SetDefault(param.Cache_EvictionMonitoringInterval.GetName(), 60)
+	v.SetDefault(param.Cache_EvictionMonitoringInterval.GetName(), "60s")
 	v.SetDefault(param.Cache_EvictionMonitoringMaxDepth.GetName(), 1)
 	v.SetDefault(param.IssuerKey.GetName(), filepath.Join(configDir, "issuer.jwk"))
 	v.SetDefault(param.IssuerKeysDirectory.GetName(), filepath.Join(configDir, "issuer-keys"))
