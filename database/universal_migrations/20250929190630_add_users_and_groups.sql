6900761283706942aae8981a27caad321d6e78f2
Patrick Brophy <patrick.brophy2@gmail.com>
2025-10-10 19:43:54 +0000

    Added foreign key reference to group members table

--- database/universal_migrations/20250929190630_add_users_and_groups.sql
+++ database/universal_migrations/20250929190630_add_users_and_groups.sql
@@ -17,7 +17,10 @@ CREATE TABLE group_members (
     user_id TEXT NOT NULL,
     added_by TEXT NOT NULL,
     added_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (group_id, user_id)
+    PRIMARY KEY (group_id, user_id),
+    FOREIGN KEY (group_id) REFERENCES groups(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (added_by) REFERENCES users(id) ON DELETE CASCADE
 );
 -- +goose StatementEnd
 
