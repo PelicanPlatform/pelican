9b668c1ac1fe924ad3570cfb8a8eb6b61b7502eb
Justin Hiemstra <jhiemstra@wisc.edu>
2025-12-17 18:06:24 +0000

    Add param that disables Director's hosting of federation metadata
    There is an entire class of bugs that revolves around having the
    Director host a copy of federation metadata when it isn't also the
    root discovery URL for the federation.
    
    Adding this knob doesn't _solve_ those bugs, but it should help us
    detect them more easily by allowing us to turn of Director metadata
    when we don't want people using the Director for that purpose.

--- config/config.go
+++ config/config.go
@@ -1325,6 +1325,7 @@ func SetServerDefaults(v *viper.Viper) error {
 		v.SetDefault(param.Origin_Multiuser.GetName(), false)
 	}
 
+	v.SetDefault(param.Director_EnableFederationMetadataHosting.GetName(), true)
 	v.SetDefault(param.Director_CheckOriginPresence.GetName(), true)
 	v.SetDefault(param.Director_CheckCachePresence.GetName(), true)
 
