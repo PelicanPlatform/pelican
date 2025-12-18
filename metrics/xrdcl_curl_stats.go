015b6f9c3393fc2b9257116c7f5ea07463c574ae
Patrick Brophy <patrick.brophy2@gmail.com>
2025-12-18 19:21:14 +0000

    Add E2E test to assert that stats are being produced and metrics are being updated

--- metrics/xrdcl_curl_stats.go
+++ metrics/xrdcl_curl_stats.go
@@ -12,13 +12,19 @@ import (
 	"github.com/pelicanplatform/pelican/param"
 )
 
+var (
+	// The interval for polling the xrdcl-curl stats file
+	// This is a variable so it can be shortened in tests
+	XrdCurlStatsInterval = time.Second * 5
+)
+
 // This function should be used up until XRootD v6 is released
 // When XRootD v6 is released, these stats will be available over the g-stream
 // Until then the stats are consumed from Cache.ClientStatisticsLocation
 // When XRootD v6 is released, this function should be removed along with Cache.ClientStatisticsLocation
 func LaunchXrdCurlStatsMonitoring(ctx context.Context, egrp *errgroup.Group) {
 	egrp.Go(func() error {
-		ticker := time.NewTicker(time.Second * 5)
+		ticker := time.NewTicker(XrdCurlStatsInterval)
 		defer ticker.Stop()
 
 		for {
