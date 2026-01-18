b7766870560b07aa0ac89d3417fe923447b3afb1
Brian Bockelman <bbockelman@morgridge.org>
2026-01-18 17:26:49 -0600

    Clear broker state between test runs
    With this, broker tests can be run with `-count=2`.

--- broker/client_init.go
+++ broker/client_init.go
@@ -7,10 +7,15 @@ import (
 	"github.com/gin-gonic/gin"
 	"golang.org/x/sync/errgroup"
 
+	"github.com/pelicanplatform/pelican/server_utils"
 	"github.com/pelicanplatform/pelican/web_ui"
 )
 
 var callbackRegistered sync.Once
+
+func init() {
+	server_utils.RegisterBrokerReset(Reset)
+}
 
 // InitializeBrokerClient sets up everything needed for a service to use
 // brokered connections (as the initiator, not as the broker itself).
@@ -19,4 +24,11 @@ func InitializeBrokerClient(ctx context.Context, egrp *errgroup.Group, router *g
 		RegisterBrokerCallback(ctx, router.Group("/", web_ui.ServerHeaderMiddleware))
 	})
 	LaunchNamespaceKeyMaintenance(ctx, egrp)
+}
+
+// Reset the global state of the module; it is assumed this is invoked
+// from unit tests only without concurrency issues.
+func Reset() {
+	callbackRegistered = sync.Once{}
+	ResetState()
 }
