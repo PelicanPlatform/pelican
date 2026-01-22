a62dbd69e75fa6442807e6e8886337f96c7aa5e3
Brian Bockelman <bbockelman@morgridge.org>
2026-01-21 18:22:18 -0600

    Add xrdhttp-pelican to the GitHub Action install script for OS X

--- github_scripts/osx_install.sh
+++ github_scripts/osx_install.sh
@@ -77,7 +77,20 @@ sudo mkdir -p /etc/xrootd/client.plugins.d/
 sudo cp release_dir/etc/xrootd/client.plugins.d/{curl,pelican}-plugin.conf /etc/xrootd/client.plugins.d/
 popd
 
-git clone --recurse-submodules --branch v0.6.0 https://github.com/PelicanPlatform/xrootd-s3-http.git
+git clone https://github.com/PelicanPlatform/xrdhttp-pelican.git
+pushd xrdhttp-pelican
+git checkout v0.0.10
+mkdir build
+cd build
+cmake .. -GNinja -DCMAKE_INSTALL_PREFIX="$PWD/release_dir"
+ninja install
+xrootd_libdir=$(grealpath "$(dirname "$(grealpath "$(which xrootd)")")"/../lib/)
+echo "Will install xrdhttp-pelican into: $xrootd_libdir"
+sudo mkdir -p "$xrootd_libdir"
+sudo ln -s "$PWD/release_dir/lib/libXrdHttpPelican-5.so" "$xrootd_libdir"
+popd
+
+git clone --recurse-submodules --branch v0.6.1 https://github.com/PelicanPlatform/xrootd-s3-http.git
 pushd xrootd-s3-http
 mkdir build
 cd build
