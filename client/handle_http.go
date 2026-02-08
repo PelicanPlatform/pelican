c397b0f00d54c6681fb19e261e1997505e62e210
Brian Bockelman <bbockelman@morgridge.org>
2026-02-07 19:57:05 -0600

    Cleanup shared-vs-regular authorization
    - Shared permission implies the token should be generated for exactly
      the path -- it's shared like a "pre-signed URL", as specific as
      possible.
    - Regular permission may ask for more authorization to avoid getting
      tokens repeatedly.

--- client/handle_http.go
+++ client/handle_http.go
@@ -1249,9 +1249,9 @@ func (tc *TransferClient) NewTransferJob(ctx context.Context, remoteUrl *url.URL
 	if _, exists := copyUrl.Query()[pelican_url.QueryRecursive]; exists {
 		recursive = true
 	}
-	operation := config.TokenSharedRead
+	operation := config.TokenRead
 	if upload {
-		operation = config.TokenSharedWrite
+		operation = config.TokenWrite
 	}
 	tj = &TransferJob{
 		prefObjServers: tc.prefObjServers,
@@ -1450,7 +1450,7 @@ func (tc *TransferClient) NewPrestageJob(ctx context.Context, remoteUrl *url.URL
 		xferType:       transferTypePrestage,
 		uuid:           id,
 		project:        project,
-		token:          newTokenGenerator(&copyUrl, nil, config.TokenSharedRead, !tc.skipAcquire),
+		token:          NewTokenGenerator(&copyUrl, nil, config.TokenRead, !tc.skipAcquire),
 	}
 	if tc.token != "" {
 		tj.token.SetToken(tc.token)
@@ -1565,7 +1565,7 @@ func (tc *TransferClient) CacheInfo(ctx context.Context, remoteUrl *url.URL, opt
 	}
 
 	var prefObjServers []*url.URL
-	token := newTokenGenerator(pelicanURL, nil, config.TokenSharedRead, true)
+	token := NewTokenGenerator(pelicanURL, nil, config.TokenRead, true)
 	if tc.token != "" {
 		token.SetToken(tc.token)
 	}
