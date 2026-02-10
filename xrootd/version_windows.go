d69c1e1590784a1232397ef5c51a0e2dbeba0d8c
Emma Turetsky <turetske@gmail.com>
2026-02-10 16:31:16 +0000

    Removed version.go and version_test.go from windows builds
    -- Windows versions do not start xrootd servers or build with xrootd, so
    remove unneeded packages and tests
    -- Create a dummy windows version for the build

---
+++ xrootd/version_windows.go
@@ -0,0 +1,28 @@
+//go:build windows
+
+/***************************************************************
+ *
+ * Copyright (C) 2026, Pelican Project, Morgridge Institute for Research
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ***************************************************************/
+
+package xrootd
+
+// CheckXrootdVersion is a no-op on Windows since XRootD is not supported on this platform.
+// Cache and origin servers are not available on Windows.
+func CheckXrootdVersion() error {
+	// No-op on Windows - XRootD is not supported
+	return nil
+}
