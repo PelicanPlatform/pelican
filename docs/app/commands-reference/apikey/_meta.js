11769396a31250c948861afd3c0ac3c9a92797ed
Patrick Brophy <patrick.brophy2@gmail.com>
2026-02-27 22:33:36 +0000

    Run go generate

---
+++ docs/app/commands-reference/apikey/_meta.js
@@ -0,0 +1,3 @@
+export default {
+    "generate": "pelican apikey generate",
+}
---
+++ docs/app/commands-reference/origin/collection/acl/_meta.js
@@ -0,0 +1,5 @@
+export default {
+    "grant": "pelican origin collection acl grant",
+    "list": "pelican origin collection acl list",
+    "revoke": "pelican origin collection acl revoke",
+}
