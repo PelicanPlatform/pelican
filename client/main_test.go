8fe6c81729cb61a4057d23bec7b4a602f0d8b289
Brian Bockelman <bbockelman@morgridge.org>
2026-02-28 07:44:35 -0600

    Client modifications needed for the local cache
    The refactored local cache work uses the client for all its
    "heavy lifting"; the goal is to keep all download, upload, and stat
    work centralized into a single location so any bugs (or improvements)
    only need to be written once.
    
    Accordingly, we really had to "bulk up" some of the functionality
    of the client to make it work better as a library and not just a
    download/upload tool.
    
    The two potentially-breaking changes in this work are:
    
    - Director responses are cached by the transfer engine in a TTLCache.
      Previously, each separate item transferred incurred the cost of a
      director lookup.
    - The internal "stat" function prefers to "stat" against a cache in
      cases where we will be downloading from the cache.  If the object
      turns out to be a collection, it'll redo the stat with the collections
      URL.
    
    Other important changes include:
    
    - Makes the `ETag` and other headers evailable to the client even before
      the data transfer starts.  This will be used by the cache to determine
      where / how to store the object.
    - Some `ETag` awareness in the download paths: if different caches provide
      the same object with different `ETag`s, abort the transfer.
    - Downloads can be done for a given byte-range instead of a whole object.
    - If the PUT response includes an ETag, then this is included in the
      transfer results returned to the client.
    - Uploads and downloads can be done against a Reader/Writer instead of
      a file on the filesystem.
    - The client can be put in "cache mode", contacting origins directly
      even for origins that refuse direct reads from end-users.  For
      obvious reasons, this is not exposed to the CLI.
    - The client can provide a second, "federation token", that the cache
      uses to provide it's not an end-user to origins that reject.
    - Token generation is now an interface, `TokenProvider`, in case the
      caller has a separate implementation of the logic.
    
    Further, this dulls the edge on - but doesn't fix - a medium-sized
    design flaw in the client: both the transfer client and transfer engine
    try to close the results channel as if they own it.  So, if you are
    careless about the ordering of shutdown, you can trigger a panic on
    close.  This fixes the panic but not the underlying "shared ownership"
    problem.

--- client/main_test.go
+++ client/main_test.go
@@ -160,7 +160,7 @@ func TestGetToken(t *testing.T) {
 
 	// ENVs to test: BEARER_TOKEN, BEARER_TOKEN_FILE, XDG_RUNTIME_DIR/bt_u<uid>, TOKEN, _CONDOR_CREDS/scitoken.use, .condor_creds/scitokens.use
 	os.Setenv("BEARER_TOKEN", "bearer_token_contents")
-	token := NewTokenGenerator(pUrl, &dirResp, config.TokenSharedWrite, false)
+	token := newTokenGenerator(pUrl, &dirResp, config.TokenSharedWrite, false)
 	tokenContents, err := token.Get()
 	assert.NoError(t, err)
 	assert.Equal(t, "bearer_token_contents", tokenContents)
@@ -174,7 +174,7 @@ func TestGetToken(t *testing.T) {
 	err = os.WriteFile(bearer_token_file, tmpFile, 0644)
 	assert.NoError(t, err)
 	os.Setenv("BEARER_TOKEN_FILE", bearer_token_file)
-	token = NewTokenGenerator(pUrl, &dirResp, config.TokenSharedWrite, false)
+	token = newTokenGenerator(pUrl, &dirResp, config.TokenSharedWrite, false)
 	tokenContents, err = token.Get()
 	assert.NoError(t, err)
 	assert.Equal(t, token_contents, tokenContents)
@@ -187,7 +187,7 @@ func TestGetToken(t *testing.T) {
 	err = os.WriteFile(bearer_token_file, tmpFile, 0644)
 	assert.NoError(t, err)
 	os.Setenv("XDG_RUNTIME_DIR", tmpDir)
-	token = NewTokenGenerator(pUrl, &dirResp, config.TokenSharedWrite, false)
+	token = newTokenGenerator(pUrl, &dirResp, config.TokenSharedWrite, false)
 	tokenContents, err = token.Get()
 	assert.NoError(t, err)
 	assert.Equal(t, token_contents, tokenContents)
@@ -200,7 +200,7 @@ func TestGetToken(t *testing.T) {
 	err = os.WriteFile(bearer_token_file, tmpFile, 0644)
 	assert.NoError(t, err)
 	os.Setenv("TOKEN", bearer_token_file)
-	token = NewTokenGenerator(pUrl, &dirResp, config.TokenSharedWrite, false)
+	token = newTokenGenerator(pUrl, &dirResp, config.TokenSharedWrite, false)
 	tokenContents, err = token.Get()
 	assert.NoError(t, err)
 	assert.Equal(t, token_contents, tokenContents)
@@ -213,7 +213,7 @@ func TestGetToken(t *testing.T) {
 	err = os.WriteFile(bearer_token_file, tmpFile, 0644)
 	assert.NoError(t, err)
 	os.Setenv("_CONDOR_CREDS", tmpDir)
-	token = NewTokenGenerator(pUrl, &dirResp, config.TokenSharedWrite, false)
+	token = newTokenGenerator(pUrl, &dirResp, config.TokenSharedWrite, false)
 	tokenContents, err = token.Get()
 	assert.NoError(t, err)
 	assert.Equal(t, token_contents, tokenContents)
@@ -234,7 +234,7 @@ func TestGetToken(t *testing.T) {
 			Namespace: "/user/ligo/frames",
 		},
 	}
-	token = NewTokenGenerator(pUrl, &ligoDirResp, config.TokenSharedRead, false)
+	token = newTokenGenerator(pUrl, &ligoDirResp, config.TokenSharedRead, false)
 	tokenContents, err = token.Get()
 	assert.NoError(t, err)
 	assert.Equal(t, token_contents, tokenContents)
@@ -250,7 +250,7 @@ func TestGetToken(t *testing.T) {
 	os.Setenv("_CONDOR_CREDS", tmpDir)
 	pUrl, err = pelican_url.Parse("renamed.handle1+osdf:///user/ligo/frames", nil, nil)
 	assert.NoError(t, err)
-	token = NewTokenGenerator(pUrl, &ligoDirResp, config.TokenSharedRead, false)
+	token = newTokenGenerator(pUrl, &ligoDirResp, config.TokenSharedRead, false)
 	tokenContents, err = token.Get()
 	assert.NoError(t, err)
 	assert.Equal(t, token_contents, tokenContents)
@@ -266,7 +266,7 @@ func TestGetToken(t *testing.T) {
 	os.Setenv("_CONDOR_CREDS", tmpDir)
 	pUrl.RawScheme = "renamed.handle2+osdf"
 	assert.NoError(t, err)
-	token = NewTokenGenerator(pUrl, &ligoDirResp, config.TokenSharedRead, false)
+	token = newTokenGenerator(pUrl, &ligoDirResp, config.TokenSharedRead, false)
 	tokenContents, err = token.Get()
 	assert.NoError(t, err)
 	assert.Equal(t, token_contents, tokenContents)
@@ -282,7 +282,7 @@ func TestGetToken(t *testing.T) {
 	os.Setenv("_CONDOR_CREDS", tmpDir)
 	pUrl.RawScheme = "renamed.handle3+osdf"
 	assert.NoError(t, err)
-	token = NewTokenGenerator(pUrl, &ligoDirResp, config.TokenSharedRead, false)
+	token = newTokenGenerator(pUrl, &ligoDirResp, config.TokenSharedRead, false)
 	tokenContents, err = token.Get()
 	assert.NoError(t, err)
 	assert.Equal(t, token_contents, tokenContents)
@@ -298,7 +298,7 @@ func TestGetToken(t *testing.T) {
 	os.Setenv("_CONDOR_CREDS", tmpDir)
 	pUrl, err = pelican_url.Parse("osdf:///user/ligo/frames", nil, nil)
 	assert.NoError(t, err)
-	token = NewTokenGenerator(pUrl, &ligoDirResp, config.TokenSharedRead, false)
+	token = newTokenGenerator(pUrl, &ligoDirResp, config.TokenSharedRead, false)
 	token.SetTokenName("renamed")
 	tokenContents, err = token.Get()
 	assert.NoError(t, err)
@@ -317,7 +317,7 @@ func TestGetToken(t *testing.T) {
 	assert.NoError(t, err)
 	err = os.Chdir(tmpDir)
 	assert.NoError(t, err)
-	token = NewTokenGenerator(pUrl, &dirResp, config.TokenSharedWrite, false)
+	token = newTokenGenerator(pUrl, &dirResp, config.TokenSharedWrite, false)
 	tokenContents, err = token.Get()
 	assert.NoError(t, err)
 	assert.Equal(t, token_contents, tokenContents)
@@ -325,7 +325,7 @@ func TestGetToken(t *testing.T) {
 	assert.NoError(t, err)
 
 	// Check that we haven't regressed on our error messages
-	token = NewTokenGenerator(pUrl, &dirResp, config.TokenSharedWrite, false)
+	token = newTokenGenerator(pUrl, &dirResp, config.TokenSharedWrite, false)
 	_, err = token.Get()
 	var pe *error_codes.PelicanError
 	require.ErrorAs(t, err, &pe)
