97831b5295f7ca8e82e2350aa9b193d8e19b78e7
Justin Hiemstra <jhiemstra@wisc.edu>
2026-03-04 19:11:14 +0000

    Rebase and regenerate docs code

---
+++ docs/app/commands-reference/server/database/backup/_meta.js
@@ -0,0 +1,7 @@
+export default {
+    "create": "pelican server database backup create",
+    "info": "pelican server database backup info",
+    "list": "pelican server database backup list",
+    "restore": "pelican server database backup restore",
+    "verify": "pelican server database backup verify",
+}
