8cb0db0d11b7670be307bb8fef2d3077b4ee4941
Justin Hiemstra <jhiemstra@wisc.edu>
2026-03-04 18:55:43 +0000

    Make 'web_ui/frontend/out/placeholder' part of repo to avoid build issues
    I wasn't a fan of how many different places we ended up having to touch
    this file in our build process to run code generators or produce binaries.
    
    Rather than make building Pelican harder, let's just add the placeholder
    to our repo and ignore anything ele in `web_ui/frontend/out`.

--- generate/next_generator.go
+++ generate/next_generator.go
@@ -23,27 +23,7 @@ package main
 import (
 	"log"
 	"os"
-	"path/filepath"
 )
-
-// Generate a placeholder file under web_ui/frontend/out so that
-// we can build Go without error. This is mainly for the linter
-// GitHub Action that doesn't need frontend to be built. Refer to
-// linter GHA for details.
-func GenPlaceholderPathForNext() {
-	dir := "../web_ui/frontend/out"
-	if err := os.MkdirAll(dir, 0755); err != nil {
-		log.Fatalf("error: %v", err)
-	}
-
-	filePath := filepath.Join(dir, "placeholder")
-
-	file, err := os.Create(filePath)
-	if err != nil {
-		log.Fatalf("error: %v", err)
-	}
-	file.Close()
-}
 
 // Copy swagger document from swagger/pelican-swagger.yaml to the web_ui/frontend/app/api directory
 func GenSwaggerDoc() {
