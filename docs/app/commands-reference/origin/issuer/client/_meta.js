2d12d8683f29001c46dd68b4b26ef90c877d74fe
Brian Bockelman <bbockelman@morgridge.org>
2026-03-07 22:17:49 -0600

    Add new documentation pages

---
+++ docs/app/commands-reference/origin/issuer/client/_meta.js
@@ -0,0 +1,6 @@
+export default {
+    "create": "pelican origin issuer client create",
+    "delete": "pelican origin issuer client delete",
+    "list": "pelican origin issuer client list",
+    "update": "pelican origin issuer client update",
+}
