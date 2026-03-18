372316e8871d3ddd81572bf8a9025713c514094e
Brian Bockelman <bbockelman@morgridge.org>
2026-03-17 21:21:27 -0500

    Director should provide typical headers for /pelican/monitoring namespace
    Improve handling of these redirects so they do not cause the client/local-cache
    to fail.

--- client/handle_http.go
+++ client/handle_http.go
@@ -2571,7 +2571,7 @@ func downloadObject(transfer *transferFile) (transferResults TransferResults, er
 		downloaded += attemptDownloaded
 
 		if err != nil {
-			log.WithFields(fields).Debugln("Failed to download from", transferEndpoint.Url, ":", err)
+			log.WithFields(fields).Debugln("Failed to download from", transferEndpoint.Url.String(), ":", err)
 			proxyStr, _ := os.LookupEnv("http_proxy")
 			if !transferEndpoint.Proxy {
 				proxyStr = ""
