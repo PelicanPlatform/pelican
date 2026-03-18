b976dd07c7948a5b52639c07e74f583f6d7baa53
Brian Bockelman <bbockelman@morgridge.org>
2026-03-18 08:28:31 -0500

    Add introspect commands to documentation

---
+++ docs/app/commands-reference/cache/introspect/_meta.js
@@ -0,0 +1,6 @@
+export default {
+    "etags": "pelican cache introspect etags",
+    "list": "pelican cache introspect list",
+    "metadata": "pelican cache introspect metadata",
+    "verify": "pelican cache introspect verify",
+}
