1c565c5e0f31df8afae62a43a68e5475ee6cc0b8
Emma Turetsky <turetske@gmail.com>
2026-03-23 15:42:51 +0000

    Define ADIOS storage type constant
    Add OriginStorageAdios = "adios" to the OriginStorageType enum and wire
    it into ParseOriginStorageType. Update the error message to list adios
    among the known storage types.

--- server_structs/origin.go
+++ server_structs/origin.go
@@ -28,6 +28,7 @@ const (
 	OriginStoragePosix    OriginStorageType = "posix"
 	OriginStoragePosixv2  OriginStorageType = "posixv2"
 	OriginStorageSSH      OriginStorageType = "ssh"
+	OriginStorageAdios    OriginStorageType = "adios"
 	OriginStorageS3       OriginStorageType = "s3"
 	OriginStorageHTTPS    OriginStorageType = "https"
 	OriginStorageGlobus   OriginStorageType = "globus"
@@ -54,6 +55,8 @@ func ParseOriginStorageType(storageType string) (ost OriginStorageType, err erro
 		ost = OriginStoragePosixv2
 	case string(OriginStorageSSH):
 		ost = OriginStorageSSH
+	case string(OriginStorageAdios):
+		ost = OriginStorageAdios
 	case string(OriginStorageXRoot):
 		ost = OriginStorageXRoot
 	case string(OriginStorageGlobus):
@@ -65,7 +68,7 @@ func ParseOriginStorageType(storageType string) (ost OriginStorageType, err erro
 	case string(OriginStorageGlobusv2):
 		ost = OriginStorageGlobusv2
 	default:
-		err = errors.Wrapf(ErrUnknownOriginStorageType, "storage type %s (known types are posix, posixv2, ssh, s3, s3v2, https, httpsv2, globus, globusv2, and xroot)", storageType)
+		err = errors.Wrapf(ErrUnknownOriginStorageType, "storage type %s (known types are posix, posixv2, ssh, adios, s3, s3v2, https, httpsv2, globus, globusv2, and xroot)", storageType)
 	}
 	return
 }
