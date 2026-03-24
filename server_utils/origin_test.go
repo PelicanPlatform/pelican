cb2da8e5138baf7d7c8fdab1a41e6fab2c7cab05
Justin Hiemstra <jhiemstra@wisc.edu>
2026-03-24 15:39:24 +0000

    Lint

--- server_utils/origin_test.go
+++ server_utils/origin_test.go
@@ -669,15 +669,15 @@ func TestFederationPrefixValidation(t *testing.T) {
 	runFedPrefixTest(t, "/origins/example.org", false)
 	runFedPrefixTest(t, "/caches/foo/bar", false) // Test prefix for caches
 	runFedPrefixTest(t, "/caches/example.org", false)
-	runFedPrefixTest(t, "/pelican", false)         // Test reserved /pelican prefix
-	runFedPrefixTest(t, "/pelican/foo", false)     // Test reserved /pelican sub-path
-	runFedPrefixTest(t, "/view", false)            // Test reserved /view prefix
-	runFedPrefixTest(t, "/view/foo", false)        // Test reserved /view sub-path
-	runFedPrefixTest(t, "/api", false)             // Test reserved /api prefix
-	runFedPrefixTest(t, "/api/v1.0/test", false)   // Test reserved /api sub-path
-	runFedPrefixTest(t, "/.well-known", false)     // Test reserved /.well-known prefix
+	runFedPrefixTest(t, "/pelican", false)                          // Test reserved /pelican prefix
+	runFedPrefixTest(t, "/pelican/foo", false)                      // Test reserved /pelican sub-path
+	runFedPrefixTest(t, "/view", false)                             // Test reserved /view prefix
+	runFedPrefixTest(t, "/view/foo", false)                         // Test reserved /view sub-path
+	runFedPrefixTest(t, "/api", false)                              // Test reserved /api prefix
+	runFedPrefixTest(t, "/api/v1.0/test", false)                    // Test reserved /api sub-path
+	runFedPrefixTest(t, "/.well-known", false)                      // Test reserved /.well-known prefix
 	runFedPrefixTest(t, "/.well-known/openid-configuration", false) // Test reserved /.well-known sub-path
-	runFedPrefixTest(t, "/valid/prefix", true)     // Test valid prefix
+	runFedPrefixTest(t, "/valid/prefix", true)                      // Test valid prefix
 }
 
 func runPathLikePrefixTest(t *testing.T, name string, valid bool) {
@@ -703,14 +703,14 @@ func TestPathLikePrefixValidation(t *testing.T) {
 	runPathLikePrefixTest(t, "/backslash\\test", false) // Test prefix with '\'
 
 	// Federation-reserved prefixes should be VALID for storage prefixes
-	runPathLikePrefixTest(t, "/pelican", true)          // /pelican is only reserved in the federation
-	runPathLikePrefixTest(t, "/pelican/foo", true)      // sub-paths of /pelican are valid storage prefixes
-	runPathLikePrefixTest(t, "/view", true)             // /view is only reserved in the federation
-	runPathLikePrefixTest(t, "/view/foo", true)         // sub-paths of /view are valid storage prefixes
-	runPathLikePrefixTest(t, "/origins/foo/bar", true)  // /origins is only reserved in the federation
-	runPathLikePrefixTest(t, "/caches/foo/bar", true)   // /caches is only reserved in the federation
-	runPathLikePrefixTest(t, "/api", true)              // /api is only reserved in the federation
-	runPathLikePrefixTest(t, "/api/v1.0/test", true)    // sub-paths of /api are valid storage prefixes
-	runPathLikePrefixTest(t, "/.well-known", true)      // /.well-known is only reserved in the federation
-	runPathLikePrefixTest(t, "/valid/prefix", true)     // Test valid prefix
+	runPathLikePrefixTest(t, "/pelican", true)         // /pelican is only reserved in the federation
+	runPathLikePrefixTest(t, "/pelican/foo", true)     // sub-paths of /pelican are valid storage prefixes
+	runPathLikePrefixTest(t, "/view", true)            // /view is only reserved in the federation
+	runPathLikePrefixTest(t, "/view/foo", true)        // sub-paths of /view are valid storage prefixes
+	runPathLikePrefixTest(t, "/origins/foo/bar", true) // /origins is only reserved in the federation
+	runPathLikePrefixTest(t, "/caches/foo/bar", true)  // /caches is only reserved in the federation
+	runPathLikePrefixTest(t, "/api", true)             // /api is only reserved in the federation
+	runPathLikePrefixTest(t, "/api/v1.0/test", true)   // sub-paths of /api are valid storage prefixes
+	runPathLikePrefixTest(t, "/.well-known", true)     // /.well-known is only reserved in the federation
+	runPathLikePrefixTest(t, "/valid/prefix", true)    // Test valid prefix
 }
