0f2bee642eee49c2b56d50682f58e5f02b9bebce
Brian P Bockelman <bbockelman@morgridge.org>
2026-04-11 17:44:04 +0000

    Add auto-generated files

---
+++ docs/app/commands-reference/origin/group/invite/_meta.js
@@ -0,0 +1,6 @@
+export default {
+    "create": "pelican origin group invite create",
+    "list": "pelican origin group invite list",
+    "redeem": "pelican origin group invite redeem",
+    "revoke": "pelican origin group invite revoke",
+}
