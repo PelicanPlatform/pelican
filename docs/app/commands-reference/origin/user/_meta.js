e790c01095a25538509bbf216170c10b44f59c5c
Brian P Bockelman <bbockelman@morgridge.org>
2026-04-11 17:44:04 +0000

    Add auto-generated files

---
+++ docs/app/commands-reference/origin/user/_meta.js
@@ -0,0 +1,7 @@
+export default {
+    "create": "pelican origin user create",
+    "delete": "pelican origin user delete",
+    "identity": "pelican origin user identity",
+    "list": "pelican origin user list",
+    "set-status": "pelican origin user set-status",
+}
