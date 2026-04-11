cc7c2351a8826d35c62ed98f7a176e4ab6834af0
Brian P Bockelman <bbockelman@morgridge.org>
2026-04-11 17:44:04 +0000

    Add auto-generated files

--- docs/app/commands-reference/origin/_meta.js
+++ docs/app/commands-reference/origin/_meta.js
@@ -1,9 +1,11 @@
 export default {
     "collection": "pelican origin collection",
     "config": "pelican origin config",
+    "group": "pelican origin group",
     "issuer": "pelican origin issuer",
     "serve": "pelican origin serve",
     "ssh-auth": "pelican origin ssh-auth",
     "token": "pelican origin token",
+    "user": "pelican origin user",
     "web-ui": "pelican origin web-ui",
 }
---
+++ docs/app/commands-reference/origin/group/_meta.js
@@ -0,0 +1,9 @@
+export default {
+    "add-member": "pelican origin group add-member",
+    "create": "pelican origin group create",
+    "delete": "pelican origin group delete",
+    "invite": "pelican origin group invite",
+    "list": "pelican origin group list",
+    "remove-member": "pelican origin group remove-member",
+    "set-ownership": "pelican origin group set-ownership",
+}
