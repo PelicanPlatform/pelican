turetske/etpelican

Wire ADIOS backend into origin server
- handlers.go: instantiate adiosBackend in InitializeHandlers for the
  OriginStorageAdios case, reading service URL and optional auth token
  file from config
- launchers/origin_serve.go: add OriginStorageAdios to the useXRootD
  exclusion list in both OriginServe and OriginServeFinish so XRootD
  is not started and does not intercept ADIOS requests
- origin/advertise.go: include OriginStorageAdios in the native-backend
  condition so the director is advertised the gin data URL, not the
  XRootD port
- cmd/main.go: add a helpful config-file example to the CLI error path
  for the adios storage type

launchers/origin_serve.go
@@ -60,7 +60,8 @@ func OriginServe(ctx context.Context, engine *gin.Engine, egrp *errgroup.Group, 
 		storageType != string(server_structs.OriginStorageSSH) &&
 		storageType != string(server_structs.OriginStorageS3v2) &&
 		storageType != string(server_structs.OriginStorageHTTPSv2) &&
-		storageType != string(server_structs.OriginStorageGlobusv2)
+		storageType != string(server_structs.OriginStorageGlobusv2) &&
+		storageType != string(server_structs.OriginStorageAdios)
 
 	if useXRootD {
 		metrics.SetComponentHealthStatus(metrics.OriginCache_XRootD, metrics.StatusWarning, "XRootD is initializing")
@@ -232,7 +233,8 @@ func OriginServeFinish(ctx context.Context, egrp *errgroup.Group, engine *gin.En
 		storageType != string(server_structs.OriginStorageSSH) &&
 		storageType != string(server_structs.OriginStorageS3v2) &&
 		storageType != string(server_structs.OriginStorageHTTPSv2) &&
-		storageType != string(server_structs.OriginStorageGlobusv2)
+		storageType != string(server_structs.OriginStorageGlobusv2) &&
+		storageType != string(server_structs.OriginStorageAdios)
 	if !useXRootD {
 		// For SSH backend, initialize the SSH connection before setting up handlers
 		if storageType == string(server_structs.OriginStorageSSH) {

PelicanPlatform/pelican

launchers/origin_serve.go
@@ -56,7 +56,9 @@ func OriginServe(ctx context.Context, engine *gin.Engine, egrp *errgroup.Group, 
 
 	// Determine if we should use XRootD or native HTTP server
 	storageType := param.Origin_StorageType.GetString()
-	useXRootD := storageType != string(server_structs.OriginStoragePosixv2) && storageType != string(server_structs.OriginStorageSSH)
+	useXRootD := storageType != string(server_structs.OriginStoragePosixv2) &&
+		storageType != string(server_structs.OriginStorageSSH) &&
+		storageType != string(server_structs.OriginStorageAdios)
 
 	if useXRootD {
 		metrics.SetComponentHealthStatus(metrics.OriginCache_XRootD, metrics.StatusWarning, "XRootD is initializing")
@@ -225,7 +227,9 @@ func OriginServeFinish(ctx context.Context, egrp *errgroup.Group, engine *gin.En
 
 	// Handle POSIXv2 and SSH-specific initialization now that the web server is running
 	storageType := param.Origin_StorageType.GetString()
-	useXRootD := storageType != string(server_structs.OriginStoragePosixv2) && storageType != string(server_structs.OriginStorageSSH)
+	useXRootD := storageType != string(server_structs.OriginStoragePosixv2) &&
+		storageType != string(server_structs.OriginStorageSSH) &&
+		storageType != string(server_structs.OriginStorageAdios)
 	if !useXRootD {
 		// For SSH backend, initialize the SSH connection before setting up handlers
 		if storageType == string(server_structs.OriginStorageSSH) {