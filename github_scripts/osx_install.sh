ee2505df5c706a7715e36d0d94b47e4e60ea3d81
Matyas Selmeci <mselmeci@wisc.edu>
2026-07-13 14:15:15 -0500

    Bump xrootd version to 6.1.0 (taken from osg-upcoming-testing); also bump xrootd-s3-http version to 0.6.8 and stop using the osg-contrib repo

--- github_scripts/osx_install.sh
+++ github_scripts/osx_install.sh
@@ -59,7 +59,7 @@ popd
 # Add patches to xrootd source code if needed
 git clone https://github.com/PelicanPlatform/xrootd.git
 pushd xrootd
-git checkout v5.9.6-pelican
+git checkout v6.1.0-pelican
 mkdir xrootd_build
 cd xrootd_build
 cmake .. -GNinja
