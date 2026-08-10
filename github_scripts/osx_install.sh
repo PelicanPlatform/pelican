e85b03b777b91d8c50e5e4eeb0d739363a2c9b64
Matyas Selmeci <mselmeci@wisc.edu>
2026-08-10 16:02:21 -0500

    Bump xrootd-s3-http dependency to 0.6.9

--- github_scripts/osx_install.sh
+++ github_scripts/osx_install.sh
@@ -90,7 +90,7 @@ sudo mkdir -p "$xrootd_libdir"
 sudo ln -s "$PWD/release_dir/lib/libXrdHttpPelican-5.so" "$xrootd_libdir"
 popd
 
-git clone --recurse-submodules --branch v0.6.7 https://github.com/PelicanPlatform/xrootd-s3-http.git
+git clone --recurse-submodules --branch v0.6.9 https://github.com/PelicanPlatform/xrootd-s3-http.git
 pushd xrootd-s3-http
 # xrootd-s3-http builds with -Wall -Werror and bundles nlohmann/json 3.11.2,
 # whose `operator "" _json` (a space before the literal suffix) trips the
