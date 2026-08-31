069ae95bd6b8a0a0f763faee94e6d88a04d84890
clock <clock@wisc.edu>
2026-08-31 15:27:13 -0500

    Add Groups/Users/Collections/Shares Documentation
    Signed-off-by: clock <clock@wisc.edu>

---
+++ docs/app/managing-data-access/_meta.js
@@ -0,0 +1,6 @@
+export default {
+    "access-control-model": "How Access Control Works",
+    "users-and-groups": "Managing Users and Groups",
+    "collections-and-shares": "Collections and Shares",
+    "permissions-reference": "Permissions Reference",
+}
