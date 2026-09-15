76e16c71719fe767f0aac6fa2c7044ee50780d83
Matyas Selmeci <mselmeci@wisc.edu>
2026-09-15 17:34:24 -0500

    Update xrdhttp-pelican to 0.0.13 in Dockerfile and OSX

--- github_scripts/osx_install.sh
+++ github_scripts/osx_install.sh
@@ -79,7 +79,7 @@ popd
 
 git clone https://github.com/PelicanPlatform/xrdhttp-pelican.git
 pushd xrdhttp-pelican
-git checkout v0.0.11
+git checkout v0.0.13
 mkdir build
 cd build
 cmake .. -GNinja -DCMAKE_INSTALL_PREFIX="$PWD/release_dir"
